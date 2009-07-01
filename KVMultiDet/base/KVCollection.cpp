7cb2c2342177211206334912fcec6a8160a4b9e7
John Frankland <frankland@ganil.fr>
2009-07-01 17:01:43 +0200

    implements methods : 	FindObjectByType/Label/Number/Class 	At (only if embedded list is TSeqCollection) 	FindObjectWithMethod and implements signals & slots mechanism sending out 'Modified()' signal whenever the contents of the list change.

--- KVMultiDet/base/KVCollection.cpp
+++ KVMultiDet/base/KVCollection.cpp
@@ -3,6 +3,9 @@
 
 #include "KVCollection.h"
 #include "TClass.h"
+#include "TROOT.h"
+#include "KVBase.h"
+#include "TMethodCall.h"
 
 ClassImp(KVCollection)
 
@@ -10,7 +13,7 @@ ClassImp(KVCollection)
 // BEGIN_HTML <!--
 /* -->
 <h2>KVCollection</h2>
-<h4>KaliVeda extension to ROOT collections</h4>
+<h4>KaliVeda extensions to ROOT collections</h4>
 This class adds functionalities such as
 <ul>
 <li>FindObjectByType</li>
@@ -24,61 +27,66 @@ KVCollection* my_coll = new KVCollection("THashList");
 </code>
 Any collection class derived from TCollection is valid.<br>
 Any object derived from TObject can be stored in the collection, but only objects
-derived from KVBase can be sought using their type or label attribute (these characteristics
-are not defined for TObject).
+derived from KVBase can be sought using their 'type', 'label', or 'number' attributes
+(these characteristics are not defined for TObject).
 <!-- */
 // --> END_HTML
 ////////////////////////////////////////////////////////////////////////////////
 
 KVCollection::KVCollection()
 {
-   // Default constructor
-   fCollection=0;
+    // Default constructor
+    fCollection=0;
 }
 
 KVCollection::KVCollection(const Char_t* collection_classname)
 {
-   // Create new extended collection of class "collection_classname".
-   // Must be the name of a class derived from TCollection.
+    // Create new extended collection of class "collection_classname".
+    // Must be the name of a class derived from TCollection.
 
-   fCollection=0;
-   TClass* cl = TClass::GetClass(collection_classname);
-   if(!cl){
-       Error("KVCollection(const Char_t*)", "Called for unknown class: %s",
-       collection_classname);
-       MakeZombie();
-       return;
-   }
-   if(!cl->InheritsFrom("TCollection")){
-       Error("KVCollection(const Char_t*)", "Called for class %s which does not inherit from TCollection",
-       collection_classname);
-       MakeZombie();
-       return;
-   }
-   fCollection = (TCollection*)cl->New();
+    fCollection=0;
+    TClass* cl = TClass::GetClass(collection_classname);
+    if (!cl)
+    {
+        Error("KVCollection(const Char_t*)", "Called for unknown class: %s",
+              collection_classname);
+        MakeZombie();
+        return;
+    }
+    if (!cl->InheritsFrom("TCollection"))
+    {
+        Error("KVCollection(const Char_t*)", "Called for class %s which does not inherit from TCollection",
+              collection_classname);
+        MakeZombie();
+        return;
+    }
+    fCollection = (TCollection*)cl->New();
 }
 
 KVCollection::~KVCollection()
 {
-   // Destructor
+    // Destructor
 }
 
 void KVCollection::Add(TObject *obj)
 {
     // Add an object to the list.
     fCollection->Add(obj);
+    Changed();
 }
 
 void KVCollection::Clear(Option_t *option)
 {
     // Clear the list of objects.
     fCollection->Clear(option);
+    Changed();
 }
 
 void KVCollection::Delete(Option_t *option)
 {
     // Delete all heap-based objects in the list.
     fCollection->Delete(option);
+    Changed();
 }
 
 TObject** KVCollection::GetObjectRef(const TObject *obj) const
@@ -97,4 +105,160 @@ TObject* KVCollection::Remove(TObject *obj)
 {
     // Remove object from list.
     return fCollection->Remove(obj);
+    Changed();
+}
+
+void	KVCollection::PrintCollectionHeader(Option_t* option) const
+{
+    // Overrides TCollection::PrintCollectionHeader to show the class name of the embedded list
+    TROOT::IndentLevel();
+    printf("Collection name='%s', embedded list class='%s', size=%d\n", GetName(),
+           fCollection->ClassName(), GetSize());
+}
+
+TObject *KVCollection::At(Int_t idx) const
+{
+    // If the embedded list inherits from TSeqCollection, this method will return the
+    // object with the given index in the list. Otherwise we return 0x0.
+
+    TSeqCollection *sc = dynamic_cast<TSeqCollection*>(fCollection);
+    if (sc) return sc->At(idx);
+    return 0;
+}
+
+TObject* KVCollection::FindObjectByType(const Char_t* type) const
+{
+    // Will return object with given type (value of KVBase::GetType() method).
+    // Objects in list which do not inherit KVBase do not have GetType() method,
+    // and are ignored.
+
+    TIter next(fCollection);
+    TObject *obj;
+    while ( (obj = next()) )
+    {
+        if ( obj->TestBit(KVBase::kIsKaliVedaObject) )
+        {
+            if ( ((KVBase*)obj)->IsType(type) )
+                return obj;
+        }
+    }
+    return 0;
+}
+
+TObject* KVCollection::FindObjectByClass(const TClass* cl) const
+{
+    // Return (first) object in embedded list with given class.
+
+    TIter next(fCollection);
+    TObject *obj;
+    while ( (obj = next()) )
+    {
+        if ( obj->IsA() == cl ) return obj;
+    }
+    return 0;
+}
+
+TObject* KVCollection::FindObjectByClass(const Char_t* cl) const
+{
+    // Return (first) object in embedded list with given class.
+
+    return FindObjectByClass( TClass::GetClass(cl) );
+}
+
+TObject* KVCollection::FindObjectByLabel(const Char_t* label) const
+{
+    // Will return object with given label (value of KVBase::GetLabel() method).
+    // Objects in list which do not inherit KVBase do not have GetLabel() method,
+    // and are ignored.
+
+    TIter next(fCollection);
+    TObject *obj;
+    while ( (obj = next()) )
+    {
+        if ( obj->TestBit(KVBase::kIsKaliVedaObject) )
+        {
+            if ( !strcmp(((KVBase*)obj)->GetLabel(), label ) ) return obj;
+        }
+    }
+    return 0;
+}
+
+
+TObject* KVCollection::FindObjectByNumber(UInt_t num) const
+{
+    // Will return object with given number (value of KVBase::GetNumber() method).
+    // Objects in list which do not inherit KVBase do not have GetNumber() method,
+    // and are ignored.
+
+    TIter next(fCollection);
+    TObject *obj;
+    while ( (obj = next()) )
+    {
+        if ( obj->TestBit(KVBase::kIsKaliVedaObject) )
+        {
+            if ( ((KVBase*)obj)->GetNumber() == num ) return obj;
+        }
+    }
+    return 0;
+}
+
+TObject *KVCollection::FindObjectWithNameAndType(const Char_t * name, const Char_t * type) const
+{
+    // Return object with specified name AND type (value of KVBase::GetType() method).
+    // Objects in list which do not inherit KVBase do not have GetType() method,
+    // and are ignored.
+
+    TIter next(fCollection);
+    TObject *obj;
+    while ( (obj = next()) )
+    {
+        if ( obj->TestBit(KVBase::kIsKaliVedaObject) && !strcmp(obj->GetName(),name) )
+        {
+            if ( !strcmp(((KVBase*)obj)->GetType(), type ) ) return obj;
+        }
+    }
+    return 0;
+}
+
+//_______________________________________________________________________________
+TObject *KVCollection::FindObjectWithMethod(const Char_t* retvalue,const Char_t* method) const
+{
+	// Find the first object in the list for which the given method returns the given return value:
+	//   e.g. if method = "GetName" and retvalue = "john", we return the
+	//   first object in this list for which GetName() returns "john".
+	//
+   // For each object of the list, the existence of the given method is checked using TMethodCall::IsValid()
+	// if the method is valid and the return value is equal to the input one (retvalue) object is returned
+	// Supported return types are those defined in TMethodCall::ReturnType()
+
+	if (retvalue && method) {
+		KVString RV(retvalue); KVString MTH(method);
+		Bool_t wildcard = RV.Contains("*");
+      TIter next(fCollection); TObject* obj;
+      while ( (obj = next()) ) {
+			TMethodCall mt;
+         mt.InitWithPrototype(obj->IsA(), MTH.Data(),"");
+			if (mt.IsValid()) {
+				if (mt.ReturnType()==TMethodCall::kString){
+					Char_t *ret;
+					mt.Execute(obj,"",&ret);
+					if (!wildcard) {if (RV==ret) { return obj; } }
+					else { if (KVString(ret).Match(RV)) { return obj; } }
+				}
+				else if (mt.ReturnType()==TMethodCall::kLong){
+					Long_t ret;
+					mt.Execute(obj,"",ret);
+					if (ret==RV.Atoi()) { return obj; }
+				}
+				else if (mt.ReturnType()==TMethodCall::kDouble){
+					Double_t ret;
+					mt.Execute(obj,"",ret);
+					if (ret==RV.Atof()) { return obj; }
+				}
+				else Error("FindObjectWithMethod", "Return type %s is not supported", mt.ReturnType());
+			}
+	   }
+   }
+	return 0;
+
 }
--- KVMultiDet/base/KVCollection.h
+++ KVMultiDet/base/KVCollection.h
@@ -5,26 +5,70 @@
 #define __KVCOLLECTION_H
 
 #include "TCollection.h"
+#include <RQ_OBJECT.h>
 
 class KVCollection : public TCollection
 {
+    RQ_OBJECT("KVList")
+
+    enum
+    {
+        kSignals = BIT(14) // bit flag for sending 'Modified()' signal on changes
+    };
+
     KVCollection(const KVCollection &) : TCollection() {};    //private and not-implemented, collections
     void operator=(const KVCollection &){}; //are too complex to be automatically copied
 
-    protected:
+protected:
     TCollection* fCollection;//Pointer to embedded ROOT collection
 
-    public:
+    virtual void Changed()
+    {
+        // Called by Add/RemoveObject, Clear and Delete.
+        // We make it emit the "Modified()" signal, if
+        // the kSignals bit has been set using SendModifiedSignals(kTRUE).
+
+        if (TestBit(kSignals)) Modified();
+    };
+    virtual void	PrintCollectionHeader(Option_t* option) const;
+
+public:
     KVCollection();
     KVCollection(const Char_t* collection_classname);
     virtual ~KVCollection();
 
+    virtual void Modified()
+    {
+        Emit("Modified()");
+    };  // *SIGNAL*
+    virtual void SendModifiedSignals(Bool_t yes = kTRUE)
+    {
+        // yes=kTRUE: turns on signals-and-slots mechanism, list will
+        //           emit 'Modified()' signal every time a change
+        //           occurs (objects added or removed).
+        // yes=kFALSE: turns off signals-and-slots mechanism
+        //
+        // By default, the 'Modified()' signal is NOT enabled.
+        SetBit(kSignals,yes);
+    };
+    virtual Bool_t IsSendingModifiedSignals()
+    {
+        // returns kTRUE if 'Modified()' signal is active
+        // (see SendModifiedSignals).
+        return TestBit(kSignals);
+    };
+
+    virtual TObject*	At(Int_t idx) const;
     virtual void       Add(TObject *obj);
     virtual void       Clear(Option_t *option="");
     virtual void       Delete(Option_t *option="");
     virtual TObject  **GetObjectRef(const TObject *obj) const;
     virtual TIterator *MakeIterator(Bool_t dir = kIterForward) const;
     virtual TObject   *Remove(TObject *obj);
+    virtual Int_t GetSize() const
+    {
+        return fCollection->GetSize();
+    };
 
     const Char_t* CollectionClassName() const
     {
@@ -32,7 +76,19 @@ class KVCollection : public TCollection
         return fCollection->ClassName();
     };
 
-   ClassDef(KVCollection,1)//KaliVeda extension to ROOT collections
+    virtual TObject *FindObjectByName(const Char_t *name) const
+    {
+        return fCollection->FindObject(name);
+    };
+    virtual TObject *FindObjectByType(const Char_t *) const;
+    TObject *FindObjectByClass(const Char_t *) const;
+    TObject *FindObjectByClass(const TClass *) const;
+    virtual TObject *FindObjectByLabel(const Char_t *) const;
+    virtual TObject *FindObjectByNumber(UInt_t num) const;
+    virtual TObject *FindObjectWithNameAndType(const Char_t * name, const Char_t * type) const;
+    virtual TObject *FindObjectWithMethod(const Char_t* retvalue,const Char_t* method) const;
+
+    ClassDef(KVCollection,1)//KaliVeda extensions to ROOT collections
 };
 
 #endif
