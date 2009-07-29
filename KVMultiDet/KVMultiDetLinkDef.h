f4a810ce357e64c9a0f9234506fe89a1765f1091
John Frankland <frankland@ganil.fr>
2009-07-29 15:01:38 +0200

    pragma links for SRB classes were only enabled on CCALI machines, outside of ccin2p3 this leads to errors when trying to link with libKVMultiDet:
    g++ -O2 -m32 analyse_logs.o -L/home/frankland/.root-v5.22.00_gcc-4.3.3_i486/lib -lCore -lCint -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -pthread -lm -ldl -rdynamic  -L/home/frankland/.kvroot-1.7_gcc-4.3.3_i486_root-5.22.00/lib -lKVMultiDet -o analyse_logs
    /home/frankland/.kvroot-1.7_gcc-4.3.3_i486_root-5.22.00/lib/libKVMultiDet.so: undefined reference to `SRBFile_t::ShowMembers(TMemberInspector&, char*)'
    /home/frankland/.kvroot-1.7_gcc-4.3.3_i486_root-5.22.00/lib/libKVMultiDet.so: undefined reference to `SRBDataRepository::Streamer(TBuffer&)'
    /home/frankland/.kvroot-1.7_gcc-4.3.3_i486_root-5.22.00/lib/libKVMultiDet.so: undefined reference to `SRBFile_t::Streamer(TBuffer&)'
    /home/frankland/.kvroot-1.7_gcc-4.3.3_i486_root-5.22.00/lib/libKVMultiDet.so: undefined reference to `SRBDataRepository::ShowMembers(TMemberInspector&, char*)'
    
    etc. etc.

--- KVMultiDet/KVMultiDetLinkDef.h
+++ KVMultiDet/KVMultiDetLinkDef.h
@@ -211,10 +211,8 @@ $Id: KVMultiDetLinkDef.h,v 1.136 2009/04/15 11:44:53 ebonnet Exp $
 #pragma link C++ class KVRelativeVelocity;
 #pragma link C++ class KVUpdateChecker;
 #pragma link C++ class KVCVSUpdateChecker;
-#ifdef __CCIN2P3_SRB
 #pragma link C++ class SRB+;
 #pragma link C++ class SRBFile_t+;
 #pragma link C++ class SRBDataRepository+;
 #pragma link C++ class SRBAvailableRunsFile+;
-#endif
 #endif
