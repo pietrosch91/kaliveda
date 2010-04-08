00e36578b651f708a3177532a1c4bc2956dac121
John Frankland <frankland@ganil.fr>
2010-04-08 09:40:12 +0200

    implements new analysis class for INDRA-VAMOS data (INDRa_e494s & INDRA_e503) which includes access to VAMOS data structures

--- VAMOS/VAMOSLinkDef.h
+++ VAMOS/VAMOSLinkDef.h
@@ -32,4 +32,5 @@ $Id: VAMOSLinkDef.h,v 1.4 2009/01/14 15:59:49 franklan Exp $
 #pragma link C++ class KVSiliconVamos+;
 #pragma link C++ class KVIDSiCsIVamos+;
 #pragma link C++ class KVFocalPlanVamos+;
+#pragma link C++ class KVIVSelector+;
 #endif
---
+++ VAMOS/factory/KVIVSelectorTemplate.h
@@ -0,0 +1,14 @@
+class KVIVSelectorTemplate : public KVIVSelector {
+
+   public:
+   KVIVSelectorTemplate() {};
+   virtual ~KVIVSelectorTemplate() {};
+   
+   virtual void InitRun();
+   virtual void EndRun();
+   virtual void InitAnalysis();
+   virtual Bool_t Analysis();
+   virtual void EndAnalysis();
+
+   ClassDef(KVIVSelectorTemplate,0);
+};
