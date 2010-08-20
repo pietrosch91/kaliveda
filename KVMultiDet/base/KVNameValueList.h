283e404e2b6ee55be4b9d29c8ac9175cd9163215
Eric Bonnet <ebonnet@ccali26>
2010-08-20 16:49:18 +0200

    ajout de diverses methodes, line avec KVGenParList

--- KVMultiDet/base/KVNameValueList.h
+++ KVMultiDet/base/KVNameValueList.h
@@ -7,6 +7,7 @@
 #include "TList.h"
 #include "TNamed.h"
 #include "KVString.h"
+#include "KVGenParList.h"
 
 class KVNameValueList : public TList
 {
@@ -65,8 +66,27 @@ class KVNameValueList : public TList
 		return (TNamed* )FindObject(name);
 	}
 	
+	Bool_t HasParameter(const Char_t* name){
+		if (FindTNamed(name)) return kTRUE;
+		else return kFALSE;
+	}
+	
 	Int_t GetNameIndex(const Char_t* name){
-		return IndexOf(FindTNamed(name));
+		TNamed* named = 0;
+		if (!(named = FindTNamed(name))){
+			Error("GetNameIndex","\"%s\" does not correspond to an existing parameter, default value -1 is returned",name);
+			return -1;
+		}
+		return IndexOf(named);
+	}
+	
+	const Char_t* GetNameAt(Int_t idx){
+		TNamed* named = 0;
+		if (!(named = (TNamed* )At(idx))){
+			Error("GetNameAt","index has to be less than %d, empty string is returned",idx);
+			return "";
+		}
+		return named->GetName();
 	}
 	
 	Int_t GetIntValue(const Char_t* name){ 
@@ -78,9 +98,44 @@ class KVNameValueList : public TList
 		return sconvert.Atof();
 	}
 	const Char_t* GetStringValue(const Char_t* name){ 
-		return FindTNamed(name)->GetTitle(); 
+		TNamed* named = 0;
+		if (!(named = FindTNamed(name))){
+			Error("GetStringValue(const Char_t*)","\"%s\" does not correspond to an existing parameter, default value -1 is returned",name);
+			return "-1";
+		}
+		return named->GetTitle(); 
 	}
-
+	Int_t GetNpar(){
+		return GetEntries();
+	}
+	Int_t GetIntValue(Int_t idx){ 
+		sconvert.Form("%s",GetStringValue(idx)); 
+		return sconvert.Atoi();
+	}
+	Double_t GetDoubleValue(Int_t idx){ 
+		sconvert.Form("%s",GetStringValue(idx)); 
+		return sconvert.Atof();
+	}
+	const Char_t* GetStringValue(Int_t idx){ 
+		if (idx>=GetNpar()) {
+			Error("GetStringValue(Int_t)","index has to be less than %d, default value -1 is returned",GetNpar());
+			return "-1";
+		}
+		return At(idx)->GetTitle(); 
+	}
+	
+	void Convert(KVGenParList& gen){
+		for (Int_t np=0;np<gen.GetNPar();np+=1){
+			SetValue(gen.GetParameter(np)->GetName(),gen.GetParameter(np)->GetVal());
+		}
+	}
+	
+	void Transfert(KVNameValueList& lnv){
+		for (Int_t np=0;np<lnv.GetNpar();np+=1){
+			SetValue( lnv.GetNameAt(np), lnv.GetStringValue(np) );
+		}
+	}
+	
    ClassDef(KVNameValueList,1)//list of TNamed
 };
 
