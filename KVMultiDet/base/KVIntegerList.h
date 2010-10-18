18b4ddd08f1429f2221c9feff8e5e78feea8dccd
Eric Bonnet <Unknown>
2010-10-18 19:06:02 +0200

    KVPartition devient classe fille de KVIntegerList

--- KVMultiDet/base/KVIntegerList.h
+++ KVMultiDet/base/KVIntegerList.h
@@ -6,154 +6,86 @@
 #include "KVString.h"
 #include "TArrayI.h"
 #include "TNamed.h"
-#include "TObjArray.h"
-#include "TObjString.h"
-#include "TMath.h"
 
 class KVEvent;
-class KVPartition;
 
 class KVIntegerList : public TNamed
 {
 	
 	protected:
 	
-	TArrayI* fRegle;	//!
-	TArrayI* fValues;	//!
-	Int_t fLimiteRegle;	//!
-	Int_t fPop;	//!
-	Int_t fMult;//!
-	Ssiz_t fLength; //!
+	TArrayI* fRegle;	//!		tableau d'entiers où sont stockée l'occurence des valeurs
+	Int_t fLimiteRegle;	//!->	taille max de fRegle
+	Int_t fPop;	//!				population de la liste/partition consideree, permet le comptage de partitions identiques dansun lot de donée
+	Int_t fMult;//!				Nombre d'éléments dans la liste
+	Ssiz_t fLength; //!			Longueur du nom de la liste/partition usilisée dans Compare
 	
 	virtual void init();
-	Bool_t ToBeUpdated() { return TestBit(kHastobeComputed); } 
+	Bool_t ToBeUpdated();
 	virtual void Update();
-	virtual void EraseRegle(){ if (fRegle) delete fRegle; fRegle=0; } 
-	virtual void EraseValues(){ if (fValues) delete fValues; fValues=0; } 
-	
-	virtual void DeducePartitionFromTNamed(){
-		DeducePartitionFromName();
-		DeducePopulationFromTitle();
-	}
-	
-	virtual void DeducePopulationFromTitle(){
-		KVString st(GetTitle());
-		SetPopulation(st.Atoi());
-	}
-	
-	virtual void DeducePartitionFromName(){
-		SetPartition(GetName());
-	}
+	virtual void ResetRegle();
 	
 	virtual void SetPartition(const Char_t* par);
+	virtual void DeducePartitionFromTNamed();
+	virtual void DeducePopulationFromTitle();
+	virtual void DeducePartitionFromName();
+	
+	virtual void add_values(Int_t val,Int_t freq);
+	virtual Bool_t remove_values(Int_t val,Int_t freq);
+	
 	
 	public:
 	
 	enum {
-      kHastobeComputed = BIT(14)	//Variables has to be recalculated
+      kHastobeComputed = BIT(14)	//Variables has to be recalculated or not
   	};
 	
 	KVIntegerList();
 	virtual ~KVIntegerList();
 	
-	Int_t Compare(KVIntegerList* par) {
-		
-		/*
-		CheckForUpdate();
-		par->CheckForUpdate();
-		*/
-		const char *s1 = this->GetName();
-   	const char *s2 = par->GetName();
-   	
-		Ssiz_t len1 = this->GetLengthName();
-   	
-		if (len1 != par->GetLengthName()) return -1;
-   	
-		Int_t ii=0;
-		while (s1[ii]==s2[ii]){
-			if (ii==len1) return 0;
-			ii+=1;
-		}
-		return -1;
-	}
+	Int_t Compare(const TObject* obj) const;
+	void Clear(Option_t* option = "");
+	void Copy(TObject& named) const;
+	void Print(Option_t* option = "") const;
 	
-	virtual void ResetPartition(){ 	
-		if (fRegle) {
-			for (Int_t ii=0;ii<fRegle->GetSize();ii+=1)
-				fRegle->AddAt(0,ii);
-			SetBit(kHastobeComputed,kTRUE);
-		}
-	}
+	void CheckForUpdate();
 	
-	void CheckForUpdate() {
-		if (ToBeUpdated()) 
-			Update();
-	}
-	void Fill(Int_t* tab,Int_t mult);
-	void Fill(Double_t* tab,Int_t mult);
-	void Fill(KVEvent* evt,Option_t* opt);
+	virtual void Fill(TArrayI* tab);
+	virtual void Fill(Int_t* tab,Int_t mult);
+	virtual void Fill(Double_t* tab,Int_t mult);
+	virtual void Fill(KVEvent* evt,Option_t* opt);
 	
-	void Add(Int_t* tab,Int_t mult){
-		for (Int_t ii=0;ii<mult;ii+=1)
-			Add(tab[ii]);
-	}
+	void Add(TArrayI* tab);
+	void Add(Int_t* tab,Int_t mult);
+	void Add(Double_t* tab,Int_t mult);
 	
-	void Add(Double_t* tab,Int_t mult){
-		for (Int_t ii=0;ii<mult;ii+=1)
-			Add(tab[ii]);
-	}
-	
-	void Add(Int_t val) { Add(val,1); }
+	void Add(Int_t val);
 	void Add(Int_t val,Int_t freq);
 	
-	void Add(Double_t val) { Add(TMath::Nint(val),1); }
-	void Add(Double_t val,Int_t freq){ Add(TMath::Nint(val),freq); }
+	void Add(Double_t val);
+	void Add(Double_t val,Int_t freq);
 	
+	Bool_t Remove(Int_t val,Int_t freq);
 	Bool_t Remove(Int_t val);
-	
-	//void Copy(TObject& obj) const {}
+	Bool_t RemoveAll(Int_t val);
 
-	void ResetPopulation() { fPop=0; }
-	Int_t GetPopulation() const { return fPop; }
-	void AddPopulation(Int_t pop) { fPop += pop; }
-	void SetPopulation(Int_t pop) { fPop = pop; }
+	void ResetPopulation();
+	Int_t GetPopulation() const;
+	void AddPopulation(Int_t pop);
+	void SetPopulation(Int_t pop);
 	
-	Int_t GetNbre() {/*CheckForUpdate();*/ return fMult;}
-	TArrayI* GetTableOfValues();
-	void SetTableOfValues();
+	Int_t GetNbre() const;
 	
-	Int_t GetFrequencyOf(Int_t val){
-		CheckForUpdate();
-		return (val<=fLimiteRegle ? fRegle->At(val) : -1);
-	}
-	Bool_t Contains(Int_t val){
-		return (GetFrequencyOf(val)>0);
-	}
-	
-	const char* GetPartitionName(){
-		CheckForUpdate();
-		return GetName();
-	}
-	
-	Ssiz_t GetLengthName(){
-		return fLength;
-	}
-	
-	void Reduce(){
-		EraseRegle();
-		EraseValues();
-		fLimiteRegle=0;
-	}
-	
-	void PrintPartition(){
-		Info("PrintPartition","%s : population %d",GetPartitionName(),GetPopulation());
-	}
-	
-	KVPartition* CreateKVPartition(Int_t mom_max=5);
+	Int_t GetFrequency(Int_t val) const;
+	Bool_t Contains(Int_t val) const;
+	Ssiz_t GetLengthName() const;
+
+//	KVPartition* CreateKVPartition(Int_t mom_max=5);
 	TNamed* CreateTNamed();
-	
-	
-	ClassDef(KVIntegerList,1)//Permet de gerer des partitions de nombres entiers
+	TArrayI* CreateTArrayI();
+	Int_t* CreateTableOfValues();
+
+	ClassDef(KVIntegerList,1)//Permet de gerer une liste de nombres entiers positifs
 };
 
 #endif
--- KVMultiDet/events/KVPartition.cpp
+++ KVMultiDet/events/KVPartition.cpp
@@ -4,11 +4,6 @@
 #include "KVPartition.h"
 #include "TMath.h"
 #include "Riostream.h"
-#include "KVNumberList.h"
-#include "KVEvent.h"
-#include "KVNucleus.h"
-#include "KVNameValueList.h"
-#include "KVIntegerList.h"
 #include "TArrayI.h"
 
 ClassImp(KVPartition)
@@ -17,467 +12,218 @@ ClassImp(KVPartition)
 /*
 BEGIN_HTML
 <h2>KVPartition</h2>
-<h4>Permet de gerer des partitions de nombres entiers et le calcul de grandeurs associees</h4>
+<h4>Classe dérivée de KVIntegerList</h4>
+<br>
 END_HTML
-Cette classe trouve une application dans la gestion de partition de charges 
-BEGIN_LATEX
-{Z_{i},i=1,M_{tot}}
-END_LATEX
-Elle a deux parametres d'entree qui sont accessibles par le constructeur
-Il s'agit de :
-- la taille maximale du systeme a partitionner (val_max) et/ou de la taille du plus gros cluster
-- l'ordre supérireure des moments (mommax) qui seront calcules automatiquement KVPartition::Compute()
-BEGIN_LATEX
-Moment d'ordre n : M_{n} = \Sigma_{i=1,Mtot} P( Z_{i} ) * Z_{i}^{ n}
-END_LATEX
+A la classe mere, est rajoutée l'object fValues, qui permet le calcul automatique de
+des moments d'une variable jusqu'à un ordre donné (par defaut GetOrdreMax() = 5)
+Deux TArrayI permettent de stocker les valeurs de la liste et sont remplis dans la routine Update
+- ftab ( accessible via GetValues() ) -> tableau de toutes les valeurs de la liste de dimension fMult (GetMult())
+- ftab_diff ( accessible via GetValuesDiff() ) -> tableau des valeurs différentes de la liste de dimension fMult_diff (GetMultDiff())
 
-Les methodes type Fill...(...) permettent de remplir et de classer la partition dans le meme temps
-A chaque appel de ces methodes les contenus de la classe sont reinititialises et recalcules
+Cette classe donne accès aux grandeurs calculées dans la classe KVValues KVPartition::GetAddValues()
+et aux valeurs de la liste via par exemple :
+- Double_t GetZmax(Int_t rang=0) 
+- Double_t GetZmin(Int_t rang=0) 
+- Double_t GetZ1() const 
+- Double_t GetZ2() const 
+- Double_t GetZtot() const 
+etc ....
 
-Voici un petit exemple pour remplir une KVPartition de deux facons
 
-BEGIN_HTML
-<pre>
-<code>
-void test()
-{
-Int_t* taInt_t* tab=0;
-
-KVPartition* par = new KVPartition(100,3);
-par->Fill("12 13 46 20 89");
-par->Print("Moments");
-par->Print("Partition");
-
-tab = new Int_t[12];
-for (Int_t nn=0;nn<12;nn+=1){
-	tab[nn] = TMath::Nint(gRandom->Uniform(0.9,20.1));
-}
-	par->Fill(tab,12);
-	par->Print("Moments");
-	par->Print("Partition");
-
-delete [] tab;
-delete par;
-}
-</code>
-</pre>
-END_HTML
-Des methodes type Compare...(...) permettent de comparer une partition a une autre en utilisant differents
-ingredients
-
-Les methodes type Remove..(..) permettent de retirer des valeurs et de recalculer les grandeurs a partir
-des restantes
-
-Des grandeurs calculables a partir des moments peuvent etre definies dans la methode
-KVPartition::CalculValeursAdditionnelles() en la derivant dans une classe fille
-Ces grandeurs additionnelles sont stockées dans une KVGenParList
-pour y acceder on peut utiliser les methodes GetValeursEnPlus(...)
 */
 ////////////////////////////////////////////////////////////////////////////////
 
-void KVPartition::init(Int_t valmax,Int_t mommax)
+void KVPartition::init(Int_t mommax)
 {
+//Initialisation 	
+	fValues = new KVValues("Moments",mommax);
 	
-	val_max=valmax;
-	mom_max=mommax;
+	ftab = new TArrayI(20);
+	ftab_diff = new TArrayI(20);
 	
-	population=0;
-	
-	regle = new Int_t[val_max+1];			for (Int_t mm=0; mm<=val_max; mm+=1)	regle[mm]=0;
-	moments = new Double_t[mom_max+1];	for (Int_t mm=0; mm<=mom_max; mm+=1)	moments[mm]=0.;
-	
-	nbre_val_diff=0;
-	nbre_val=0;
-	
-	valeurs_diff=0;
-	valeurs=0;
-	
-	//SetBit(kCanDelete,kTRUE);
-	
-	lgen = new KVNameValueList();
-	SetName("");
+	fMult_diff=0;
+
 }
 
-KVPartition::KVPartition(Int_t valmax,Int_t mommax)
+//___________________________________________________________________________________________
+KVPartition::KVPartition(Int_t mommax)
 {
-   // Default constructor
-	init(valmax,mommax);
+// Constructor
+	init(mommax);
 }
 
+//___________________________________________________________________________________________
 KVPartition::KVPartition()
 {
-   // Default constructor
+// Default constructor
 	init();
 }
 
+//___________________________________________________________________________________________
 KVPartition::~KVPartition()
 {
-   // Destructor
-	//Info("~KVPartition()","Je suis dans le destructeur");
-	Reset();
+//Destructeur
+   delete fValues;
+	delete ftab;
+	delete ftab_diff;
+}
+
+
+//___________________________________________________________________________________________
+void KVPartition::Clear(Option_t* option)
+{ 	
+//Methode dérivée de KVIntegerList, Reinitialisation de l'object
 	
-	delete [] regle;		regle=0;
-	delete [] moments;	moments=0;
-	delete lgen; 			lgen=0;
+	KVIntegerList::Clear();
+	
+	fValues->Reset();
+	for (Int_t ii=0;ii<ftab->fN;ii+=1) 			ftab->AddAt(ii,0);
+	for (Int_t ii=0;ii<ftab_diff->fN;ii+=1)	ftab_diff->AddAt(ii,0);
+		
+	fMult=0;
+	fMult_diff=0;
 
 }
 
-void KVPartition::Copy(TObject & obj) const{
-
-	if  ( ((KVPartition &) obj).GetMomMax()!=GetMomMax() || ((KVPartition &) obj).GetValMax()!=GetValMax() )
-	{
-		delete ((KVPartition &) obj).regle; 
-		delete ((KVPartition &) obj).moments;
-		delete ((KVPartition &) obj).lgen;
-		 
-		((KVPartition &) obj).init(GetValMax(),GetMomMax());
-	}
+//___________________________________________________________________________________________
+void KVPartition::Copy(TObject & obj) const
+{
+//Methode dérivée de KVIntegerList, fait une copie dans l'objet "obj"
+	KVIntegerList::Copy(obj);
+	((KVPartition& )obj).Update();
 	
-	((KVPartition &) obj).FillWithRegle(regle,GetValMax());
-
 }
 
-void KVPartition::Compute(){
-	
-	valeurs = new Int_t[nbre_val];   
-	valeurs_diff = new Int_t[nbre_val_diff]; 
-	
-	ComputeValues();
-	
-	SetBit(kHastobeComputed,kFALSE);
-	
-	AddOne();
+//___________________________________________________________________________________________
+void KVPartition::Update()
+{
+//protected method, Methode dérivée de KVIntegerList,
+//Appel de KVIntegerList::Update()
+//les deux TArrayI ftab et ftab_diff sont mis à jour
+//un test de dimension est fait pour etendre si besoin
 
-}
-
-void KVPartition::ComputeValues(){
-	
-	
-	KVString snom,stamp;
-	Int_t mdiff=0,mtot=0;
-	//Boucle sur les valeurs de 0 a val_max
-	for (Int_t nn=val_max; nn>=0; nn-=1){
-		if (regle[nn]>0){
-			//Calcul des moments
-			for (Int_t ordre=0; ordre<=mom_max; ordre+=1)	
-				moments[ordre] += regle[nn] * TMath::Power(nn,ordre);
-			
-			//Formattage du nom de la partition
-			stamp.Form("%d",nn);	snom += stamp;
-			if (regle[nn]>1){
-				stamp.Form("(%d)",regle[nn]);
-				snom += stamp; 
+	if (fMult>ftab->fN) 				ftab->Set(fMult); 
+	if (fMult_diff>ftab_diff->fN) ftab_diff->Set(fMult_diff); 
+	Info("Update","fMult=%d fMult_diff=%d",fMult,fMult_diff);
+	Int_t mdiff=0,mtot=0;	
+	KVString snom="",stamp="";
+	for (Int_t ii = fLimiteRegle; ii>=0; ii-=1){
+		Int_t contenu = fRegle->At(ii);
+		if (contenu>0){
+			ftab_diff->AddAt(ii,mdiff++);
+			for (Int_t mm=0;mm<contenu;mm+=1){
+				ftab->AddAt(ii,mtot++);
 			}
 			
-			//Enregistrement des valeurs
-			//dans les deux tableaux 
-			valeurs_diff[mdiff++]=nn;
-			for (Int_t mm=0;mm<regle[nn];mm+=1){
-				valeurs[mtot++]=nn;
+			//Formattage du nom de la partition
+			stamp.Form("%d",ii);	snom += stamp;
+			if (contenu>1){
+				stamp.Form("(%d)",contenu);
+				snom += stamp; 
 			}
 			snom+=" ";
 		}
 	}
-
-	SetName(snom);
+	if (snom!="") snom.Remove(snom.Length()-1);	
+	SetName(snom.Data());
+	fLength = snom.Length();
 	
-	CalculValeursAdditionnelles();
-
+	SetBit(kHastobeComputed,kFALSE);
+		
 }
 
-void KVPartition::Fill(Int_t* tab,Int_t mult){
-
-	if (!tab) return;
-	Reset();
-	
-	for (Int_t mm=0;mm<mult;mm+=1)
-		AddToRegle(tab[mm]);
-	
-	Compute();
-
-}
-
-void KVPartition::Fill(TArrayI* arr){
-
-	if (!arr) return;
-	Reset();
-	Int_t mult = arr->GetSize();
-	for (Int_t mm=0;mm<mult;mm+=1)
-		AddToRegle(arr->At(mm));
-	
-	Compute();
-
-}
-
-void KVPartition::Fill(Double_t* tab,Int_t mult){
-
-	if (!tab) return;
-	Reset();
-	
-	for (Int_t mm=0;mm<mult;mm+=1)
-		AddToRegle(TMath::Nint(tab[mm]));
-	
-	Compute();
-
-}
-
-void KVPartition::FillWithConditions(Int_t* tab,Int_t mult,Int_t zmin,Int_t zmax){
-
-	if (!tab) return;
-	Reset();
-	Bool_t select_min = ( (zmin==-1) ? kFALSE : kTRUE);
-	Bool_t select_max = ( (zmax==-1) ? kFALSE : kTRUE);
-	
-	for (Int_t mm=0;mm<mult;mm+=1)
-		if ( ( !select_min || (select_min && tab[mm]>=zmin) ) && ( !select_max || (select_max && tab[mm]<=zmax) ) ) 
-			AddToRegle(tab[mm]);
-	
-	Compute();
-
-}
-
-void KVPartition::Fill(KVNumberList nl){
-
-	Reset();
-	
-	Int_t mult;
-	Int_t* tab = nl.GetArray(mult);
-	for (Int_t mm=0;mm<mult;mm+=1)
-		AddToRegle(tab[mm]);
-	
-	delete [] tab;
-	
-	Compute();
-
-}
-
-void KVPartition::Fill(KVEvent* evt,Option_t* opt){
-
-	if (!evt) return;
-	
-	Reset();
-	
-	KVNucleus* nuc=0;
-	while ( (nuc = (KVNucleus* )evt->GetNextParticle(opt)) ) 
-		AddToRegle(nuc->GetZ());
-	
-	Compute();
-
-}
-
-void KVPartition::FillWithRegle(Int_t* regl,Int_t vmax){
-
-	Reset();
-	
-	for (Int_t mm=0;mm<=vmax;mm+=1)
-		for (Int_t nb=0;nb<regl[mm];nb+=1)
-			AddToRegle(mm);
-	
-	Compute();
-
-}
-
-void KVPartition::AddToRegle(Int_t val){
-	if (regle[val]==0)
-		nbre_val_diff+=1;
-	nbre_val+=1;
-	
-	regle[val]+=1;
-}
-
-void KVPartition::Reset(){
-
-	for (Int_t mm=0;mm<nbre_val_diff;mm+=1){
-		regle[valeurs_diff[mm]]=0;
-	}
-	
-	if (valeurs_diff) delete [] valeurs_diff; valeurs_diff=0;
-	if (valeurs) 		delete [] valeurs;		valeurs=0;
-	
-	nbre_val_diff=0;
-	nbre_val=0;
-	
-	ResetMoments();
-	ResetPopulation();
-	SetName("");
-
-}
-
-void KVPartition::ResetMoments(){
-	for (Int_t mm=0;mm<=mom_max;mm+=1) 
-		moments[mm]=0;
-}
-
+//___________________________________________________________________________________________
 void KVPartition::Print(Option_t* option) const {
-
+//Methode dérivée de KVIntegerList, imprime les informations
+//option="" -> appel uniquement de KVIntegerList::Print
+//option=="Moments", imprime les moments calculés via la classe KVValues
+//option=="Partition", imprime le nombre de valeurs GetMult() et le nombre de valeurs differentes (GetMultDiff())
+	KVIntegerList::Print(option);
 	if (!strcmp(option,"Moments")) {
 		Info("Print","Moments #Sigma Z^{ordre}");
-		printf("Nombres de moments calcules %d\n",mom_max);
-		for (Int_t mm=0;mm<=mom_max;mm+=1){
-			printf("Moments d'ordre %d -> %1.0lf\n",mm,moments[mm]);
+		Info("Print","Nombres de moments calcules %d",GetOrdreMax());
+		for (Int_t mm=0;mm<=GetOrdreMax();mm+=1){
+			Info("Print","Moments d'ordre %d -> %1.0lf",mm,GetMoment(mm));
 		}
 	}
 	else if (!strcmp(option,"Partition")) {
-		Info("Print","Partitions");
-		printf("Multiplicite %1.0lf\n",moments[0]);
-		printf("Nombre de charges differentes %d\n",nbre_val_diff);
-		for (Int_t mm=0;mm<nbre_val_diff;mm+=1){
-			printf("%d",valeurs_diff[mm]);
-			if (regle[valeurs_diff[mm]]>1)
-				printf("(%d)",regle[valeurs_diff[mm]]);
-			printf(" ");	
-		}
-		printf("\n");
-		
+		Info("Print","Multiplicite %d / Nombre de valeurs differentes %d",GetMult(),GetMultDiff());
 	}
 	else {
-		printf("KVPartition : %s Population %s\n",GetName(),GetTitle());
+	
 	}
 
 }
-
-
-Int_t KVPartition::Compare(const TObject* obj) const {
-
-	return CompareName( (KVPartition* )obj );
-
-}
-
-Int_t KVPartition::CompareMoments(KVPartition* par) const {
-	//renvoie 1 si les moments sont identiques 0 sinon
-	//Attention la comparaison ne s'eefectue que jusqu'a l'ordre max defini
-	//pour les deux partitions
-	Int_t ordremax = TMath::Min(par->GetMomentOrdreMax(),this->GetMomentOrdreMax());
-	for (Int_t ord=0; ord<=ordremax; ord+=1){
-		if (par->GetMoment(ord) != this->GetMoment(ord)) 
-			return 0;
-	}
-	return 1;
+	
+//___________________________________________________________________________________________
+void KVPartition::add_values(Int_t val,Int_t freq)
+{
+//protected method, dérivée de KVIntegerList	
+//Incrementation si besoin du nombre de valeurs differentes GetMultDiff
+//Remplissage de l'objet KVIntegerList::fValues, pour le calcul des moments		
+	if (!Contains(val)) fMult_diff+=1;
+	KVIntegerList::add_values(val,freq);
+	fValues->FillVar(val,freq);
 
 }
-
-Int_t KVPartition::CompareMult(KVPartition* par) const {
-
-	//renvoie 1 si toutes les multiplicites sont identiques 0 sinon
-	return Int_t(par->GetMult()==this->GetMult() && par->GetMultDiff()==this->GetMultDiff());
-
-}
-
-Int_t KVPartition::CompareValeurs(KVPartition* par) const {
-
-	//renvoie 1 si toutes les valeurs sont identiques 0 sinon
-	if (this->CompareMult(par)==0) return 0;
-	for (Int_t mm=0; mm<this->GetMultDiff(); mm+=1){
-		Int_t m1=this->GetValeurDiff(mm);
-		Int_t m2=par->GetValeurDiff(mm);
-		if ( (m1 != m2) || (this->GetFrequenceOf(m1) != par->GetFrequenceOf(m2)) ){
-			return 0;	
-		}
-	}
-	return 1;
-}
-
-Int_t KVPartition::CompareName(KVPartition* par) const {
-
-	//renvoie 1 si nom identique 0 sinon
-	if ( !strcmp(this->GetName(),par->GetName()) ) return 1;
-	else return 0;
+	
+//___________________________________________________________________________________________
+Bool_t KVPartition::remove_values(Int_t val,Int_t freq)
+{
+//protected method, dérivée de KVIntegerList	
+//Decrementation si besoin du nombre de valeurs differentes GetMultDiff
+//Les valeurs considerees sont également retires dans le calcul des moments, object KVIntegerList::fValues		
+		
+	if (GetFrequency(val)<=freq) fMult_diff-=1;
+	fValues->FillVar(val,-1.*freq);
+	return KVIntegerList::remove_values(val,freq);
 	
 }
 
-void KVPartition::CalculValeursAdditionnelles(){
-	
-	//Exemple de valeurs additionnelles
-	//qui peuvent etre calculees en plus
-	//cette routine peut etre redefinie dans les classes filles
-	Double_t m1 = GetMomentNormalise(1); 
-	Double_t m2 = GetMomentNormalise(2); 
-	Double_t m3 = GetMomentNormalise(3); 
-	Double_t m4 = GetMomentNormalise(4); 
-	
-	lgen->SetValue("Mean",m1);
-	lgen->SetValue("RMS",TMath::Sqrt( m2 - TMath::Power(m1,2.) ) ); //Same as TH1::GetRMS()
-	Double_t rms = lgen->GetDoubleValue("RMS");
-	//Same as TH1::GetSkewness()
-	lgen->SetValue("Skewness",(m3 - 3*m1*m2 +2*TMath::Power(m1,3.))/TMath::Power(rms,3.) );
-	//Same as TH1::GetKurtosis()+3
-	lgen->SetValue("Kurtosis",(m4 - 4*m1*m3 +6*TMath::Power(m1,2.)*m2 -3*TMath::Power(m1,4.))/TMath::Power(rms,4.) );
-	
-	lgen->SetValue("GenAsym",( (GetMoment(0)>1) ? (1./TMath::Sqrt(GetMoment(0)-1.))*(rms/m1) : -1.) );
-	
-	lgen->SetValue("Z1",GetZ1());
-	lgen->SetValue("Z2",GetZ2());
-	lgen->SetValue("Z3",GetZmax(2));
-	
-	lgen->SetValue("Ztot",GetZtot());
-	lgen->SetValue("Mdiff",GetMultDiff());
-	
-}
+//___________________________________________________________________________________________
+Double_t  KVPartition::GetZmax(Int_t rang) const 
+{
 
-Double_t  KVPartition::GetZmax(Int_t rang) const {
-	//Donne le 1er, 2eme ... plus gros de la partition
-	//Attention notation C++ 
-	// - le premier -> 0
-	// - le dernier -> N-1 ou N est la multiplicite
+//Donne le 1er, 2eme ... plus gros de la partition
+//Attention notation C
+// - le premier -> 0
+// - le dernier -> N-1 ou N est la multiplicite
 	
 	return ( (rang<GetMoment(0)) ? Double_t(GetValeur(rang)) : -1. );
 
 }
 
-Double_t  KVPartition::GetZmin(Int_t rang) const {
-	//Donne le 1er, 2eme ... plus petit de la partition
-	//Attention notation C++ 
-	// - le premier -> 0
-	// - le dernier -> N-1 ou N est la multiplicite
+//___________________________________________________________________________________________
+Double_t  KVPartition::GetZmin(Int_t rang) const 
+{
+	
+//Donne le 1er, 2eme ... plus petit de la partition
+//Attention notation C
+// - le premier -> 0
+// - le dernier -> N-1 ou N est la multiplicite
 
 	Int_t inverse = Int_t(GetMoment(0))-1-rang;
 	return ( (inverse<GetMoment(0)) ? Double_t(GetValeur(inverse)) : -1. );
 
 }
 
+//___________________________________________________________________________________________
 Bool_t KVPartition::RemoveAt(Int_t rang) {
 
-	//return kTRUE if the indicated rank is occupied
+//return kTRUE if the indicated rank is occupied
 	Int_t val = Int_t(GetZmax(rang));
 	if (val==-1) return kFALSE;
-	
-	regle[val]-=1;
-	if (regle[val]==0) {	nbre_val_diff-=1; }
-	nbre_val-=1;
-	
-	ResetMoments();
-	ComputeValues();
-	
-	return kTRUE;
+	return Remove(val);
 
 }
 
-Bool_t KVPartition::RemoveValue(Int_t val) {
+//___________________________________________________________________________________________
+Bool_t KVPartition::RemoveAllValuesAt(Int_t rang) 
+{
 
-	//return kTRUE if the indicated value belong to the partition
-	if (regle[val]>0){
-		Int_t nval = regle[val];
-		regle[val] = 0;
-		nbre_val -= nval;
-		nbre_val_diff -= 1;
-	}
-	else return kFALSE;
-	
-	ResetMoments();
-	ComputeValues();
-	
-	return kTRUE;
-
-}
-
-Double_t KVPartition::GetValeursEnPlus(KVString sname){
-
-	return GetParametersList()->GetDoubleValue(sname.Data());
-
-}
-
-Double_t KVPartition::GetValeursEnPlus(const Char_t* sname){
-
-	return GetParametersList()->GetDoubleValue(sname);
-
+//return kTRUE if the indicated rank is occupied
+	Int_t val = Int_t(GetZmax(rang));
+	if (val==-1) return kFALSE;
+	return RemoveAll(val);
 }
