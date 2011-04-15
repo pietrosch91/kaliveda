4f21683db415840b41d94c94ffdeedd3557130aa
Marie-France Rivet <rivet@ipno.in2p3.fr>
2011-04-15 14:05:55 +0200

    restreint le domaine pour calcul de la moyenne pour le Si/Si75/SiLi ne fonctionne pas pour ChIo et CsI, pics larges avec des zeros au milieu
    Pour gagner en precision, on pourrait regrouper les bins ?

--- html/examples/GetGeneMean.h
+++ html/examples/GetGeneMean.h
@@ -11,17 +11,22 @@ $Date: 2007/06/29 10:46:46 $
 #define __GETGENEMEAN_H
 
 #include "KVINDRAGeneDataSelector.h"
+//#include "KVSelector.h"
 #include "TFile.h"
 #include "TTree.h"
 #include "TH1F.h"
 
+//class GetGeneMean : public KVSelector
 class GetGeneMean : public KVINDRAGeneDataSelector
 {
    TTree* geneTree;
    TFile *geneFile;
-   Int_t run, ring, module, cigg, cipg, sipg, sigg, si75gg, si75pg, siligg, silipg, csir, csil;
    TH1F* geneHist;
+   Int_t run, ring, module, cigg, cipg, sipg, sigg, si75gg, si75pg;
+   Int_t siligg, silipg, csir, csil;
+   Int_t bininf, binsup, binmax;
    Int_t runEvents;
+   Double_t moy;
    
    public:
 
