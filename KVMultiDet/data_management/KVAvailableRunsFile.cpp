44f06177b87d40ecac350f71714e148fabf1be38
John Frankland <frankland@ganil.fr>
2012-01-12 09:58:48 +0100

    corrects 'bug' in GetListOfAvailableSystems: informations on runs (date, version, user) were only read if no specific system was given

--- KVMultiDet/data_management/KVAvailableRunsFile.cpp
+++ KVMultiDet/data_management/KVAvailableRunsFile.cpp
@@ -507,11 +507,9 @@ TList *KVAvailableRunsFile::GetListOfAvailableSystems(const KVDBSystem *
       good_lines++;
 
       fRunNumber = kvs.Atoi();
-kvversion= username="";
-      if (!systol) {
+      kvversion= username="";
          TString tmp = ((TObjString *) toks->At(1))->GetString();
          fDatime = TDatime(tmp.Data());
-      }
       if(toks->GetEntries()>3){
          kvversion = ((TObjString *) toks->At(3))->GetString();
          username = ((TObjString *) toks->At(4))->GetString();
@@ -554,7 +552,7 @@ kvversion= username="";
          if (noSystems || (systol == sys)) {   //run belongs to same system
             if (!sys_list)
                sys_list = new TList;
-            if(noSystems && a_run){
+            //if(noSystems && a_run){
 					/* Block Modified() signal being emitted by KVDBRun object
 				   when we set its 'datime'. This is to avoid seg fault with
 					KVDataAnalysisGUI, when we are changing system/display,
@@ -562,10 +560,10 @@ kvversion= username="";
 					no longer exists */
             	a_run->BlockSignals(kTRUE);
 					a_run->SetDatime(fDatime);
-            a_run->SetKVVersion(kvversion);
-            a_run->SetUserName(username);
+               a_run->SetKVVersion(kvversion);
+               a_run->SetUserName(username);
 					a_run->BlockSignals(kFALSE);
-				}
+				//}
             if(a_run) sys_list->Add(a_run);
          }
       }
