38183d042cfc2beaae91f3d84ad59954d733a61c
Rivet Marie-France <rivet@ipnnim09>
2012-05-15 10:26:14 +0200

    lecture et etalonnage des silicium VAMOS

--- VAMOS/detectors/Siv.cpp
+++ VAMOS/detectors/Siv.cpp
@@ -1,191 +1,165 @@
- #include "Siv.h"
+/*
+ *	-----------------------------------------------------------------
+ *	Siv
+ *	Initial structure from Maurycy Rejmund (2006)
+ *	-----------------------------------------------------------------
+ */
+#include "Siv.h"
 #include "KVDataSet.h"
 #include <cmath>
 
-//Author: Maurycy Rejmund
 ClassImp(Siv)
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// BEGIN_HTML <!--
-/* -->
-<h2>Siv</h2>
-<p>
-Part of the VAMOS analysis package kindly contributed by Maurycy Rejmund (GANIL).
-</p>
-<!-- */
-// --> END_HTML
-////////////////////////////////////////////////////////////////////////////////
 
 Siv::Siv(LogFile *Log)
 {
-#ifdef DEBUG
-  cout << "Si::Constructor" << endl;
-#endif
-  Ready=kFALSE;
+	#ifdef DEBUG
+	cout << "Si::Constructor" << endl;
+	#endif
+	
+	Ready=kFALSE;
   
-  char line[255];
-  int len=255;
-  int i;
-  Float_t tmp1,tmp2;
+	char line[255];
+	int len=255;
+	int i,j;
 
-  L=Log;
+	L=Log;
 
-  for(i=0;i<2;i++)
-    Counter[i] = 0;
+	for(i=0;i<2;i++) Counter[i] = 0;
 
-  InitRaw();
-  Init();
+	InitRaw();
+	Init();
 
-  Rnd = new Random;
+	Rnd = new Random;
   
-  ifstream inf;
-  
-  int tmp=0;
-  if(!gDataSet->OpenDataSetFile("Si.cal",inf))
-  {
-     cout << "Could not open the calibration file Si.cal !" << endl;
-     return;
-  }
-  else
-    {
-      cout.setf(ios::showpoint);
-      cout << "-> Reading Si.cal" << endl;
-      L->Log << "-> Reading Si.cal" << endl;
-      inf.getline(line,len);
-      cout << line << endl;
-      L->Log << line << endl;
-      while(!inf.eof())
+	ifstream inf;
+  	if(!gDataSet->OpenDataSetFile("Si.cal",inf))
 	{
-	  inf.getline(line,len);
-	  if(!inf.eof() && (tmp < 45))
-	    {
-	      if(tmp<21)
-		{
-		  sscanf(line,"%f %f %f %f %f %f %f %f", 
-		  	ECoef[tmp]+0, ECoef[tmp]+1, ECoef[tmp]+2, ECoef[tmp]+3, ECoef[tmp]+4, ECoef[tmp]+5, ECoef[tmp]+6, ECoef[tmp]+7);
-
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
-		cout << ECoef[tmp][0] <<" " << ECoef[tmp][1] <<" " << ECoef[tmp][2] <<" " 
-		<< ECoef[tmp][3] <<" " << ECoef[tmp][4] <<" " << ECoef[tmp][5] <<" " 
-		<< ECoef[tmp][6] <<" " << ECoef[tmp][7] <<" " << endl;
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
-		  
-		  tmp++;
-		}
-	      else if(tmp<42)
-		{
-		  sscanf(line,"%f %f", TOff[tmp-21]+0, TOff[tmp-21]+1);
-		  //		  cout <<  "TO " <<TOff[tmp-21][0] << " " << TOff[tmp-21][1]<< endl;
-		  tmp++;
-		}
-	      else
-		{
-		  sscanf(line,"%f %f", TCoef[tmp-42]+0, TCoef[tmp-42]+1);
-		  //		  cout <<  "TC " <<TCoef[tmp-42][0] << " " << TCoef[tmp-42][1]<< endl;
-		  tmp++;		  
-		}
-	    }
+		cout << "Could not open the calibration file Si.cal !" << endl;
+		return;
 	}
-    }
-  inf.close();
-  Ready=kTRUE;
+	else
+	{
+		cout.setf(ios::showpoint);
+		cout << "Reading Si.cal" << endl; L->Log << "Reading Si.cal" << endl;		
+/*
+ *		Silicon calibration parameters
+ */
+		inf.getline(line,len);
+		cout << line << endl; L->Log << line << endl;
+		for(i=0;i<18;i++) 
+		{
+			for(j=0;j<8;j++)  inf >> ECoef[i][j]; inf.getline(line,len);
+/*
+ *			Output control
+ */
+			printf("  silicon %2d: ",i+1);
+			for(j=0;j<8;j++) printf("  %10.4g",ECoef[i][j]); cout << endl;	
+		}
+/*	
+ *		Reading parameters for Time TSi_HF, 6 parameters a,b,c,d,e,f :
+ */
+ 		inf.getline(line,len);
+ 		cout << line << endl; L->Log << line << endl;
+ 		for(i=0;i<6;i++) inf >> TSi_HF_Coef[i];
+ 		for(i=0;i<6;i++) printf("  %10.4g",TSi_HF_Coef[i]); cout << endl ;  
+/*	
+ *		Reading t0 for Time TSi_HF, 18 values, one per silicon :
+ */
+		for(i=0;i<2;i++) inf.getline(line,len);
+		cout << line << endl; L->Log << line << endl;
+		for(i=0;i<18;i++) inf >> TSi_HF_t0[i];
+		for(i=0;i<18;i++) printf("  silicon %2d: %10.4g\n",i+1,TSi_HF_t0[i]);
+/*	
+ *		Reading parameters for Time TSi_SED1, 4 parameters a,b,c,d :
+ */
+		for(i=0;i<2;i++) inf.getline(line,len);
+		cout << line << endl; L->Log << line << endl;
+		for(i=0;i<4;i++) inf >> TSi_SED1_Coef[i];
+		for(i=0;i<4;i++) printf("  %10.4g",TSi_SED1_Coef[i]); cout << endl ;  
+/*	
+ *		Reading t0 for Time TSi_SED1, 18 values, one per silicon :
+ */
+		for(i=0;i<2;i++) inf.getline(line,len);
+		cout << line << endl; L->Log << line << endl;
+		for(i=0;i<18;i++) inf >> TSi_SED1_t0[i];
+		for(i=0;i<18;i++) printf("  silicon %2d: %10.4g\n",i+1,TSi_SED1_t0[i]);
+ 	}
+	inf.close();
+	Ready=kTRUE;
 }
+
 Siv::~Siv(void)
 {
-#ifdef DEBUG
-  cout << "Si::Destructor" << endl;
-#endif
+	#ifdef DEBUG
+	cout << "Si::Destructor" << endl;
+	#endif
   
-  PrintCounters();
+	PrintCounters();
 }
 
 void Siv::PrintCounters(void)
 {
-#ifdef DEBUG
-  cout << "Si::PrintCounters" << endl;
-#endif
+	#ifdef DEBUG
+	cout << "Si::PrintCounters" << endl;
+	#endif
   
-  cout << endl;
-  cout << "Si::PrintCounters" << endl;
-  cout << "Called: " << Counter[0] << endl;
-  cout << "Present: " << Counter[1] << endl;
+	cout << endl;
+	cout << "Si::PrintCounters" << endl;
+	cout << "Called: " << Counter[0] << endl;
+	cout << "Present: " << Counter[1] << endl;
   
-  L->Log << endl;
-  L->Log << "Si::PrintCounters" << endl;
-  L->Log << "Called: " << Counter[0] << endl;
-  L->Log << "Present: " << Counter[1] << endl;
-
+	L->Log << endl;
+	L->Log << "Si::PrintCounters" << endl;
+	L->Log << "Called: " << Counter[0] << endl;
+	L->Log << "Present: " << Counter[1] << endl;
 }
-
 
 void Siv::InitRaw(void)
 {
-#ifdef DEBUG
-  cout << "Si::InitRaw" << endl;
-#endif
+	#ifdef DEBUG
+	cout << "Si::InitRaw" << endl;
+	#endif
 
-  for(Int_t i=0;i<21;i++)
-    {
-      E_Raw[i] = 0;
-      E_Raw_Nr[i] = 0;
-    }
-  for(Int_t i=0;i<3;i++)
-    {
-      T_Raw[i] = 0;
-    }
-  E_RawM = 0;
+	for(Int_t i=0;i<21;i++) E_Raw[i] = 0;
+	for(Int_t i=0;i<21;i++) E_Raw_Nr[i] = 0;
+	for(Int_t i=0;i<3;i++) T_Raw[i] = 0;
+	E_RawM = 0;
 }
 
 void Siv::Init(void)
 {
-#ifdef DEBUG
-  cout << "Siv::Init" << endl;
-#endif
-  Present = false; 
-
-  for(Int_t i=0;i<21;i++)
-    {
-      E[i] = 0.0;
-    }
-  for(Int_t i=0;i<3;i++)
-    {
-      T[i] = 0.0;
-    }
-  Offset[0] = Offset[1]  = 0.0;
-  EM = 0;
-  ETotal = 0.0;
-  Number = 0;
-
+	#ifdef DEBUG
+	cout << "Siv::Init" << endl;
+	#endif
+	
+	Present = false; 
+	for(Int_t i=0;i<21;i++) E[i] = 0.0;
+	for(Int_t i=0;i<3;i++) T[i] = 0.0;
+	EM = 0;
+	ETotal = 0.0;
+	Number = 0;
 }
 
 void Siv::Calibrate(void)
 {
 	Int_t i,j,k;
-	double x,a,b,c,d,e,f,g,h;
+	double x,a,b,c,d,e,f,g,h,t0;
 
-#ifdef DEBUG
+	#ifdef DEBUG
 	cout << "Si::Calibrate" << endl;
-#endif
+	#endif
 
-	for(i=0;i<3;i++)
-	{
-		if(T_Raw[i] > 200)
-		for(j=0;j<2;j++)
-		{
-			T[i] = (Float_t) T_Raw[i];
-			
-//			cout << i << " " << j << " " <<TCoef[i][j] << endl;
-		}
-	}
-/*
- *	Energy calibration with Fermi functions
- */
+	for(i=0;i<3;i++) if(T_Raw[i] > 200) for(j=0;j<2;j++) 
+		T[i] = (Float_t) T_Raw[i];
+	
 	for(i=0;i<E_RawM;i++)
 	{
+		cout << "  Telescope, Mult : "  << Number << " , "<< E_RawM << endl;  
 		Rnd->Next();
-		
+/*
+ *		Calibration of silicon detectors	
+ *		Energy calibration with Fermi functions
+ */	
 		x = (double)E_Raw[i]+(double)Rnd->Value() ;
 		a = (double)ECoef[E_Raw_Nr[i]][0] ;
 		b = (double)ECoef[E_Raw_Nr[i]][1] ;
@@ -197,31 +171,52 @@ void Siv::Calibrate(void)
 		h = (double)ECoef[E_Raw_Nr[i]][7] ;
 		
 		E[E_Raw_Nr[i]] = (Float_t)( ((a*(1./(exp(b*(x+c))+1.)-1.))+(d+e*x+f*x*x))*(g/h) );		
+		cout << "    ESi[ch], [MeV] : "<< E_Raw[i] << " , "  << E[E_Raw_Nr[i]] << endl;  
 		
 		if(E[E_Raw_Nr[i]] > 0)
 		{
-			ETotal = E[E_Raw_Nr[i]];
+			ETotal += E[E_Raw_Nr[i]];
 			Number = E_Raw_Nr[i]+1;
-
-
 			EM++;
 		}
+/*
+ *		Calibration of time of flight (TSi_HF)
+ *		T[ns]= t0 - (a+(b+(c+(d+(e+f*x)*x)*x)*x)*x)*x
+ */
+		x = (double)T_Raw[0]+(double)Rnd->Value() ;
+		a = (double)TSi_HF_Coef[0] ;
+		b = (double)TSi_HF_Coef[1] ;
+		c = (double)TSi_HF_Coef[2] ;
+		d = (double)TSi_HF_Coef[3] ;
+		e = (double)TSi_HF_Coef[4] ;
+		f = (double)TSi_HF_Coef[5] ;
+		t0= (double)TSi_HF_t0[E_Raw_Nr[i]] ;
+
+		T[0] = (Float_t)( t0 -(a+(b+(c+(d+(e+f*x)*x)*x)*x)*x)*x );	
+		cout << "    TSi_HF[ch], [ns] : "<< T_Raw[0] << " , "  << T[0] << endl;  
+/*
+ *		Calibration of time of flight (TSi_SED1)
+ *		T[ns]= t0 - (a*x+b*x*x+c*exp(d*x))
+ */
+		x = (double)T_Raw[1]+(double)Rnd->Value() ;
+		a = (double)TSi_SED1_Coef[0] ;
+		b = (double)TSi_SED1_Coef[1] ;
+		c = (double)TSi_SED1_Coef[2] ;
+		d = (double)TSi_SED1_Coef[3] ;
+		t0= (double)TSi_SED1_t0[E_Raw_Nr[i]] ;
+
+		T[1] = (Float_t)( t0 - (a*x+b*x*x+c*exp(d*x)) );	
+		cout << "    TSi_SED1[ch], [ns] : "<< T_Raw[1] << " , "  << T[1] << endl;  
 	}
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
+/*
+ *	Output control
+ */
+	if(E_RawM==0) cout<<"No signal "<<endl;  
 
-			cout << Number << " , "<< ETotal << " , "<< E_RawM << endl;  
-
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
-
-  
 	if(EM!=1) 
 	{
-		ETotal = 0.0;
+		ETotal = 0.;
 		Number = 0;
-		for(k=0;k<2;k++)
-		Offset[k] = 0.0;
 	}
       
 	if(ETotal > 0) 
@@ -234,137 +229,112 @@ void Siv::Calibrate(void)
 
 void Siv::Treat(void)
 {
-#ifdef DEBUG
-  cout << "Siv::Treat" << endl;
-#endif
-  if(!Ready) return;
+	#ifdef DEBUG
+	cout << "Siv::Treat" << endl;
+	#endif
+	if(!Ready) return;
   
-  Counter[0]++;
-  Init();
-  Calibrate();
-#ifdef DEBUG
-  Show_Raw();
-  Show();
-#endif
+	Counter[0]++;
+	Init();
+	Calibrate();
+	
+	#ifdef DEBUG
+	Show_Raw();
+	Show();
+	#endif
 }
 
 void Siv::inAttach(TTree *inT)
 {
 
-#ifdef DEBUG
-  cout << "Si::inAttach" << endl;
-#endif
+	#ifdef DEBUG
+	cout << "Si::inAttach" << endl;
+	cout << "Attaching Si variables" << endl;
+	#endif
 
-#ifdef DEBUG
-  cout << "Attaching Si variables" << endl;
-#endif
+	#ifdef ACTIVEBRANCHES
+	cout << "Activating Branches: Si" << endl;
+	L->Log << "Activating Branches: Si" << endl;
+	inT->SetBranchStatus("SIENr",  1);
+	inT->SetBranchStatus("SIE",    1);
+	inT->SetBranchStatus("SIEM",   1);
+	inT->SetBranchStatus("TSI_HF", 1);
+	inT->SetBranchStatus("TSI_SED",1);
+	inT->SetBranchStatus("TSED_HF",1);
+	#endif
 
-#ifdef ACTIVEBRANCHES
-  cout << "Activating Branches: Si" << endl;
-  L->Log << "Activating Branches: Si" << endl;
-
-  inT->SetBranchStatus("SIEM",1);
-  inT->SetBranchStatus("SIE",1);
-  inT->SetBranchStatus("SIENr",1);
-
-  inT->SetBranchStatus("TSI_HF",1);
-  inT->SetBranchStatus("TSI_SED",1);
-  inT->SetBranchStatus("TSED_HF",1);
-
-  
-
-#endif
-
-  inT->SetBranchAddress("SIEM",&E_RawM);
-  inT->SetBranchAddress("SIE",E_Raw);
-  inT->SetBranchAddress("SIENr",E_Raw_Nr);
-
-  inT->SetBranchAddress("TSI_HF",T_Raw+0);
-  inT->SetBranchAddress("TSI_SED",T_Raw+1);
-  inT->SetBranchAddress("TSED_HF",T_Raw+2);
-
+	inT->SetBranchAddress("SIENr",  E_Raw_Nr);
+	inT->SetBranchAddress("SIE",    E_Raw   );
+	inT->SetBranchAddress("SIEM",   &E_RawM );
+	inT->SetBranchAddress("TSI_HF", T_Raw+0 );
+	inT->SetBranchAddress("TSI_SED",T_Raw+1 );
+	inT->SetBranchAddress("TSED_HF",T_Raw+2 );
 }
+
 void Siv::outAttach(TTree *outT)
 {
+	#ifdef DEBUG
+	cout << "Si::outAttach" << endl;
+	#endif
 
-#ifdef DEBUG
-  cout << "Si::inAttach" << endl;
-#endif
+	#ifdef DEBUG
+	cout << "Attaching Si variables" << endl;
+	#endif
 
-#ifdef DEBUG
-  cout << "Attaching Si variables" << endl;
-#endif
-
-   outT->Branch("SiEM",&EM,"SiEM/I");
-   outT->Branch("SiE",E,"SiE[21]/F");
-   outT->Branch("SiET",&ETotal,"SiET/F");
-
-   outT->Branch("SIE_Raw",E_Raw,"SiE_Raw[21]/s");
-   outT->Branch("SIE_Nr",&Number,"SIE_Nr/s");
-
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
-
-			cout << Number << " , "<< ETotal << endl;  
-
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
-// STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID STUPID 		 	
-
-   outT->Branch("TSi_HF",&T[0],"TSi_HF/F");
-   outT->Branch("TSi_SeD",&T[1],"TSi_SeD/F");
-   outT->Branch("TSeD_HF",&T[2],"TSeD_HF/F");
+	outT->Branch("SIE_Nr", &Number,"SIE_Nr/s"     );
+	outT->Branch("SiE",    E,      "SiE[21]/F"    );
+	outT->Branch("SiEM",   &EM,    "SiEM/I"       );
+//	outT->Branch("SiET",   &ETotal,"SiET/F"       );
+//	outT->Branch("SIE_Raw",E_Raw,  "SiE_Raw[21]/s");
+	outT->Branch("TSi_HF", &T[0],  "TSi_HF/F"     );
+	outT->Branch("TSi_SeD",&T[1],  "TSi_SeD/F"    );
+//	outT->Branch("TSeD_HF",&T[2],  "TSeD_HF/F"    );
 }
+
 void Siv::CreateHistograms(void)
 {
-
-#ifdef DEBUG
-  cout << "Si::CreateHistograms" << endl;
-#endif
-
+	#ifdef DEBUG
+	cout << "Si::CreateHistograms" << endl;
+	#endif
 }
+
 void Siv::FillHistograms(void)
 {
-
-#ifdef DEBUG
-  cout << "Si::FillHistograms" << endl;
-#endif
-
+	#ifdef DEBUG
+	cout << "Si::FillHistograms" << endl;
+	#endif
 }
-
 
 void Siv::Show_Raw(void)
 {
-  Int_t i;
-#ifdef DEBUG
-  cout << "Si::Show_Raw" << endl;
-#endif
+	Int_t i;
+	
+	#ifdef DEBUG
+	cout << "Si::Show_Raw" << endl;
+	#endif
   
-  cout.setf(ios::showpoint);
+	cout.setf(ios::showpoint);
 
-  for(i=0;i<E_RawM;i++)
-  {
-    cout << "CrER: " << E_Raw_Nr[i] << " " << E_Raw[i] << endl; 
-  }
-
+	for(i=0;i<E_RawM;i++)
+		cout << "CrER: " << E_Raw_Nr[i] << " " << E_Raw[i] << endl; 
 }
+
 void Siv::Show(void)
 {
-  Int_t i;
-#ifdef DEBUG
-  cout << "Si::Show" << endl;
-#endif
+	Int_t i;
+	#ifdef DEBUG
+	cout << "Si::Show" << endl;
+	#endif
 
-  cout.setf(ios::showpoint);
+	cout.setf(ios::showpoint);
  
-  cout << "Present: " << Present << endl;
-  if(Present)
-    {
-      cout << "EM: " << EM << endl;
-      for(i=0;i<21;i++)
+	cout << "Present: " << Present << endl;
+	if(Present)
 	{
-	  if(E[i]>0.) cout << "SiE: " << i << " " << E[i] << endl; 
+		cout << "EM: " << EM << endl;
+		for(i=0;i<21;i++) if(E[i]>0.) 
+			cout << "SiE: " << i << " " << E[i] << endl; 
 	}
-    }
 }
 
 
--- VAMOS/detectors/Siv.h
+++ VAMOS/detectors/Siv.h
@@ -2,66 +2,67 @@
 #define _SI_CLASS
 
 #include "Rtypes.h"
-#include"Defines.h"
-#include"LogFile.h"
-#include"Random.h"
+#include "Defines.h"
+#include "LogFile.h"
+#include "Random.h"
 #include "TTree.h"
 
 class Siv
 {
-   Bool_t Ready;
+	Bool_t Ready;
    
- public:
-  Siv(LogFile *Log);
-  virtual ~Siv(void);
+	public:
+	
+	Siv(LogFile *Log);
+	virtual ~Siv(void);
   
-  LogFile *L;
+	LogFile *L;
 
-  bool Present; 
+	bool Present; 
 
-  void Init(void); 
-  void InitRaw(void); 
-  void Calibrate(void); 
-  void Show_Raw(void);
-  void Show(void);
-  void Treat(void);
-  void inAttach(TTree *inT);
-  void outAttach(TTree *outT);
-  void CreateHistograms();
-  void FillHistograms();
-  void PrintCounters(void);
+	void Init(void); 
+	void InitRaw(void); 
+	void Calibrate(void); 
+	void Show_Raw(void);
+	void Show(void);
+	void Treat(void);
+	void inAttach(TTree *inT);
+	void outAttach(TTree *outT);
+	void CreateHistograms();
+	void FillHistograms();
+	void PrintCounters(void);
 
-  Random *Rnd;
+	Random *Rnd;
+/*
+ *	energy Raw
+ */
+	UShort_t E_Raw[18];
+	UShort_t E_Raw_Nr[18];
+	Int_t E_RawM;
+	UShort_t T_Raw[3];
+/*
+ *	Calibration coeff
+ */
+	Double_t ECoef[18][8];
+	Double_t TSi_HF_Coef[6];
+	Double_t TSi_HF_t0[18];
+	Double_t TSi_SED1_Coef[4];
+	Double_t TSi_SED1_t0[18];
+/*
+ *	energy time Calibrated
+ */
+	Int_t EM;
+	Float_t E[18];
+	UShort_t ENr[18];
+	Float_t T[3];
+	Float_t ETotal;
+	UShort_t Number; 
+/*
+ *	Counters	
+ */
+	Int_t Counter[2];
 
-  //energy Raw
-  UShort_t E_Raw[21];
-  UShort_t E_Raw_Nr[21];
-  Int_t E_RawM;
-  UShort_t T_Raw[3];
-
-
-
-  //Calibration coeff
-  Float_t ECoef[21][8];
-  Float_t TOff[21][2];
-  Float_t TCoef[3][2];
-
-  //
-
-  //energy time Calibrated
-  Int_t   EM;
-  Float_t E[21];
-  UShort_t ENr[21];
-  Float_t T[3];
-  Float_t Offset[2];
-  Float_t ETotal;
-  UShort_t Number;
- 
-
-  //Counters
-  Int_t Counter[2];
-
-  ClassDef(Siv,0)
+	ClassDef(Siv,0)
 };
 
 #endif
