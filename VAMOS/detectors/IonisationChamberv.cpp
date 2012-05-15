58beea927b995dc2cc6c32ddf5394b7d5fb4b636
Rivet Marie-France <rivet@ipnnim09>
2012-05-15 10:25:05 +0200

    lecture et etalonnage de HARPEE

--- VAMOS/detectors/IonisationChamberv.cpp
+++ VAMOS/detectors/IonisationChamberv.cpp
@@ -1,354 +1,338 @@
+/*
+ *	-----------------------------------------------------------------
+ *	IonisationChamberv
+ *	Initial structure from Maurycy Rejmund (2006)
+ *	-----------------------------------------------------------------
+ */
 #include "IonisationChamberv.h"
 #include "KVDataSet.h"
 #include <cmath>
-//Author: Maurycy Rejmund
 
 ClassImp(IonisationChamberv)
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// BEGIN_HTML <!--
-/* -->
-<h2>IonisationChamberv</h2>
-<p>
-Part of the VAMOS analysis package kindly contributed by Maurycy Rejmund (GANIL).
-</p>
-<!-- */
-// --> END_HTML
-////////////////////////////////////////////////////////////////////////////////
 
 IonisationChamberv::IonisationChamberv(LogFile *Log)
 {
-#ifdef DEBUG
-  cout << "IonisationChamberv::Constructor" << endl;
-#endif
-  Ready=kFALSE;
+#	ifdef DEBUG
+	cout << "IonisationChamberv::Constructor" << endl;
+#	endif
+	
+	Ready=kFALSE;
   
-  char line[255];
-  int len=255;
-  int i,j,k;
-  Float_t tmp;
-  Float_t tmp1;
+	char line[255];
+	int len=255;
+	int i,j,k;
+	Float_t tmp, tmp1;
 
-  L=Log;
+	L=Log;
 
-  for(i=0;i<5;i++)
-    Counter[i] = 0;
+	for(i=0;i<5;i++) Counter[i] = 0;
 
-  InitRaw();
-  Init();
+	InitRaw();
+	Init();
 
-  Rnd = new Random;
+	Rnd = new Random;
   
-  ifstream inf;
+	ifstream inf;
   
-
-  if(!gDataSet->OpenDataSetFile("IonisationChamber.cal",inf))
-  {
-     cout << "Could not open the calibration file IonisationChamber.cal !" << endl;
-     return;
-  }
-  else
-    {
-      cout.setf(ios::showpoint);
-      cout << "Reading IonisationChamber.cal" << endl;
-      L->Log << "Reading IonisationChamber.cal" << endl;
-      for(i=0;i<3;i++) 
+	if(!gDataSet->OpenDataSetFile("IonisationChamber.cal",inf))
 	{
-	  inf.getline(line,len);
-	  cout << line << endl;
-	  L->Log << line << endl;
-	  for(j=0;j<7;j++) 
-	    {
-	      for(k=0;k<3;k++) 
-		inf >> ECoef[i][j][k];
-	      
-	      inf >> tmp;
-	      inf >> tmp1;
-
-	      for(k=0;k<3;k++) 
-		ECoef[i][j][k] *= tmp;
-
-	      inf.getline(line,len);
-	    }
+		cout << "Could not open the calibration file IonisationChamber.cal !" << endl;
+		return;
 	}
-      for(j=0;j<3;j++) 
+	else
 	{
-	  inf.getline(line,len);
-	  cout << line << endl;
-	  L->Log << line << endl;
-	  for(k=0;k<2;k++) 
-	    inf >> ERef[j][k]; 
-	  inf.getline(line,len);
+		cout.setf(ios::showpoint);
+		cout << "Reading IonisationChamber.cal" << endl; L->Log << "Reading IonisationChamber.cal" << endl;
+		for(i=0;i<3;i++) 
+		{
+			inf.getline(line,len);
+			cout << line << endl; L->Log << line << endl;
+			for(j=0;j<7;j++) 
+			{
+				for(k=0;k<4;k++) inf >> ECoef[i][j][k];
+				inf.getline(line,len);
+/*
+ *				Output control
+ */
+				if(i==1) {for(k=0;k<4;k++) printf("  %10.4g",ECoef[i][j][k]); cout << endl;} 
+			}
+		}
+		for(j=0;j<3;j++) 
+		{
+			inf.getline(line,len);
+			cout << line << endl; L->Log << line << endl;
+			for(k=0;k<2;k++) inf >> ERef[j][k]; 
+			inf.getline(line,len);
+		}
+		for(j=0;j<3;j++) 
+		{
+			inf.getline(line,len);
+			cout << line << endl; L->Log << line << endl;
+			inf >> ETresh[j]; 
+			cout << "  ETresh " << ETresh[j] << endl;
+			inf.getline(line,len);
+		}
 	}
-      for(j=0;j<3;j++) 
-	{
-	  inf.getline(line,len);
-	  cout << line << endl;
-	  L->Log << line << endl;
-	  inf >> ETresh[j]; 
-	  cout << "ETresh " << ETresh[j] << endl;
-	  inf.getline(line,len);
-	}
-    }
-  inf.close();
-  Ready=kTRUE;
-  
+	inf.close();
+	Ready=kTRUE;
 }
+
 IonisationChamberv::~IonisationChamberv(void)
 {
-#ifdef DEBUG
-  cout << "IonisationChamberv::Destructor" << endl;
-#endif
+#	ifdef DEBUG
+	cout << "IonisationChamberv::Destructor" << endl;
+#	endif
   
-  PrintCounters();
+	PrintCounters();
 }
 
 void IonisationChamberv::PrintCounters(void)
 {
-#ifdef DEBUG
-  cout << "IonisationChamberv::PrintCounters" << endl;
-#endif
+#	ifdef DEBUG
+	cout << "IonisationChamberv::PrintCounters" << endl;
+#	endif
   
-  cout << endl;
-  cout << "IonisationChamberv::PrintCounters" << endl;
-  cout << "Called: " << Counter[0] << endl;
-  for(Int_t i=0;i<3;i++)
-    cout << "E" << i << ": " << Counter[i+1] << endl;
-  cout << "Present: " << Counter[4] << endl;
+	cout << endl;
+	cout << "IonisationChamberv::PrintCounters" << endl;
+	cout << "Called: " << Counter[0] << endl;
+	for(Int_t i=0;i<3;i++)
+		cout << "E" << i << ": " << Counter[i+1] << endl;
+	cout << "Present: " << Counter[4] << endl;
 
-  
-  L->Log << endl;
-  L->Log << "IonisationChamberv::PrintCounters" << endl;
-  L->Log << "Called: " << Counter[0] << endl;
-  for(Int_t i=0;i<3;i++)
-    L->Log << "E" << i << ": " << Counter[1] << endl;
-  L->Log << "Present: " << Counter[4] << endl;
-
+  	L->Log << endl;
+	L->Log << "IonisationChamberv::PrintCounters" << endl;
+	L->Log << "Called: " << Counter[0] << endl;
+	for(Int_t i=0;i<3;i++)
+		L->Log << "E" << i << ": " << Counter[1] << endl;
+	L->Log << "Present: " << Counter[4] << endl;
 }
-
 
 void IonisationChamberv::InitRaw(void)
 {
-#ifdef DEBUG
-  cout << "IonisationChamberv::InitRaw" << endl;
-#endif
-
-  for(Int_t i=0;i<3;i++)
-    for(Int_t j=0;j<8;j++)
-      {
-	E_Raw[i*8+j] = 0;
-      	E_Raw_Nr[i*8+j] = 0;
-      }
-  E_RawM = 0;
+#	ifdef DEBUG
+	cout << "IonisationChamberv::InitRaw" << endl;
+#	endif
+	
+	for(Int_t i=0;i<3;i++) for(Int_t j=0;j<8;j++)
+	{
+		E_Raw[i*8+j] = 0;
+		E_Raw_Nr[i*8+j] = 0;
+	}
+	E_RawM = 0;
 }
 
 void IonisationChamberv::Init(void)
 {
-#ifdef DEBUG
-  cout << "IonisationChamberv::Init" << endl;
-#endif
-  Present = false; 
-
-  ETotal = 0.0;
-  EM = 0;
-  for(Int_t i=0;i<3;i++)
-    for(Int_t j=0;j<8;j++)
-      {
-	E[i*8+j] = 0.0;
-      }
-
-
-  for(Int_t i =0;i<3;i++)
-    ES[i] = 0.;
-
+#	ifdef DEBUG
+	cout << "IonisationChamberv::Init" << endl;
+#	endif
+	
+	Present = false; 
+	EM = 0;
+	ETotal = 0.0;
+	Number = 0;
+	for(Int_t i=0;i<3;i++) for(Int_t j=0;j<8;j++) E[i*8+j] = 0.0;
+	for(Int_t i =0;i<3;i++) ES[i] = 0.;
 }
 
 void IonisationChamberv::Calibrate(void)
 {
-  Int_t i,k;
+	Int_t i,k;
+	double x,a,b,c,d;
+	int aa,bb;
+	int MRow[3];
 
-#ifdef DEBUG
-  cout << "IonisationChamberv::Calibrate" << endl;
-#endif
+#	ifdef DEBUG
+	cout << "IonisationChamberv::Calibrate" << endl;
+#	endif
 
-  int a,b;
-  int MRow[3];
+	MRow[0] = MRow[1] = MRow[2] = 0;
 
-  MRow[0] = MRow[1] = MRow[2] = 0;
-  for(i=0;i<E_RawM;i++)
-    {
-      a=(E_Raw_Nr[i]+1)/8;
-      b=((E_Raw_Nr[i]+1)%8)-1;
-      if(b!=-1)
+	for(i=0;i<E_RawM;i++)
 	{
-	  Rnd->Next();
-	  for(k=0;k<3;k++)
-	    {
-	      E[E_Raw_Nr[i]] += powf((Float_t) E_Raw[i] + Rnd->Value(),
-			    (Float_t) k)*ECoef[a][b][k];
-	    }
-	  E[E_Raw_Nr[i]] *= (ERef[a][1]/ERef[a][0]);
-	  if(E[E_Raw_Nr[i]] > ETresh[a])
-	    {
-	      ES[a] += E[E_Raw_Nr[i]];
-	      MRow[a] ++;
-	      ETotal += E[E_Raw_Nr[i]];
-	      //	      ENr[EM] = E_Raw_Nr[i];
-	      EM++;
-	    }
+/*
+ *		We only take the stage 2 because the others are not defined
+ */		
+//		aa=(E_Raw_Nr[i]+1)/8;
+		aa = 1;
+		bb=((E_Raw_Nr[i]+1)%8)-1;		
+		Number = bb+1;
+
+		if(bb!=-1)
+		{
+			Rnd->Next();
+	
+			x = (double)E_Raw[i]+(double)Rnd->Value() ;
+			a = (double)ECoef[aa][bb][0] ;
+			b = (double)ECoef[aa][bb][1] ;
+			c = (double)ECoef[aa][bb][2] ;
+			d = (double)ECoef[aa][bb][3] ;
 	  
-	}
-
+			E[E_Raw_Nr[i]] = (Float_t)((a + b*x) * (c / d)); 
+		
+			E[E_Raw_Nr[i]] *= (ERef[aa][1]/ERef[aa][0]);
+	  
+			if(E[E_Raw_Nr[i]] > ETresh[aa])
+			{
+				ES[aa] += E[E_Raw_Nr[i]];
+				MRow[aa] ++;
+				ETotal += E[E_Raw_Nr[i]];
+				EM++;
+			}
+/*
+ *			Output control
+ */
+			cout<<"  Telescope, ETotal, Echannel, Mult : "<<Number<<" , "<<ETotal<<" , "<<E_Raw[i]<<" , "<<E_RawM<<endl;  
+		}
     }
+/*
+ *	Output control
+ */
+	if(E_RawM==0) cout<<"No signal "<<endl;  
 
-  for(i=0;i<3;i++)
-    if(ES[i] > 0) Counter[i+1]++;
+	for(i=0;i<3;i++) if(ES[i] > 0) Counter[i+1]++;
   
-  if(ES[0] > 0. &&  ETotal > 0.0 )//&& MRow[0] == 1 && MRow[1] ==1 )
-    {
-      Present = true; 
-      Counter[4]++;
-    }
-  else
-      Init();
+	if(EM!=1)
+	{
+		ETotal = 0.;
+		Number = 0;
+	}
+	if(ES[0] > 0. &&  ETotal > 0. )
+	{
+		Present = true; 
+		Counter[4]++;
+	}
 }
 
 void IonisationChamberv::Treat(void)
 {
-#ifdef DEBUG
-  cout << "IonisationChamberv::Treat" << endl;
-#endif
-  if(!Ready) return;
-  Counter[0]++;
-  Init();
-  Calibrate();
-#ifdef DEBUG
-  Show_Raw();
-  Show();
-#endif
+	#ifdef DEBUG
+	cout << "IonisationChamberv::Treat" << endl;
+	#endif
+	
+	if(!Ready) return;
+	Counter[0]++;
+	Init();
+	Calibrate();
+
+	#ifdef DEBUG
+	Show_Raw();
+	Show();
+	#endif
 }
 
 void IonisationChamberv::inAttach(TTree *inT)
 {
 
-#ifdef DEBUG
-  cout << "IonisationChamberv::inAttach" << endl;
-#endif
+	#ifdef DEBUG
+	cout << "IonisationChamberv::inAttach" << endl;
+	#endif
 
-#ifdef DEBUG
-  cout << "Attaching IonisationChamberv variables" << endl;
-#endif
+	#ifdef DEBUG
+	cout << "Attaching IonisationChamberv variables" << endl;
+	#endif
 
-#ifdef ACTIVEBRANCHES
-  cout << "Activating Branches: IonisationChamberv" << endl;
-  L->Log << "Activating Branches: IonisationChamberv" << endl;
+	#ifdef ACTIVEBRANCHES
+	cout << "Activating Branches: IonisationChamberv" << endl;
+	L->Log << "Activating Branches: IonisationChamberv" << endl;
+	inT->SetBranchStatus("EchiNr",1);
+	inT->SetBranchStatus("Echi",  1);
+	inT->SetBranchStatus("EchiM", 1); 
+	#endif
 
-  inT->SetBranchStatus("EchiM",1);
-  inT->SetBranchStatus("Echi",1);
-  inT->SetBranchStatus("EchiNr",1);
-
-#endif
-
-      inT->SetBranchAddress("EchiM",&E_RawM);
-      inT->SetBranchAddress("Echi",E_Raw);
-      inT->SetBranchAddress("EchiNr",E_Raw_Nr);
-
+	inT->SetBranchAddress("EchiNr",E_Raw_Nr);
+	inT->SetBranchAddress("Echi",  E_Raw   );
+	inT->SetBranchAddress("EchiM", &E_RawM ); 
 }
+
 void IonisationChamberv::outAttach(TTree *outT)
 {
+	#ifdef DEBUG
+	cout << "IonisationChamberv::outAttach" << endl;
+	#endif
 
-#ifdef DEBUG
-  cout << "IonisationChamberv::outAttach" << endl;
-#endif
+	#ifdef DEBUG
+	cout << "Attaching IonisationChamberv variables" << endl;
+	#endif
 
-#ifdef DEBUG
-  cout << "Attaching IonisationChamberv variables" << endl;
-#endif
-  outT->Branch("IcEA",&ES[0],"IcEA/F");
-  outT->Branch("IcEB",&ES[1],"IcEB/F");
-  outT->Branch("IcEC",&ES[2],"IcEC/F");
-  outT->Branch("IcET",&ETotal,"IcET/F");
-
-  outT->Branch("IcM",&EM,"IcM/I");
-  outT->Branch("IcE",E,"IcE[24]/F");
-
+	outT->Branch("EchiNr",&Number,"EchiNr/s" );
+	outT->Branch("Echi",  &ETotal,"Echi/F"   );
+	outT->Branch("EchiM", &EM,    "EchiM/I"  );
+//	outT->Branch("IcE",   E,      "IcE[24]/F");
 }
+
 void IonisationChamberv::CreateHistograms(void)
 {
-
-#ifdef DEBUG
-  cout << "IonisationChamberv::CreateHistograms" << endl;
-#endif
-
+	#ifdef DEBUG
+	cout << "IonisationChamberv::CreateHistograms" << endl;
+	#endif
 }
+
 void IonisationChamberv::FillHistograms(void)
 {
-
-#ifdef DEBUG
-  cout << "IonisationChamberv::FillHistograms" << endl;
-#endif
-
+	#ifdef DEBUG
+	cout << "IonisationChamberv::FillHistograms" << endl;
+	#endif
 }
-
 
 void IonisationChamberv::Show_Raw(void)
 {
-  Int_t i;
-#ifdef DEBUG
-  cout << "IonisationChamberv::Show_Raw" << endl;
-#endif
+	Int_t i;
+	
+	#ifdef DEBUG
+	cout << "IonisationChamberv::Show_Raw" << endl;
+	#endif
   
-  cout.setf(ios::showpoint);
+	cout.setf(ios::showpoint);
   
-int a,b;
+	int aa,bb;
 
- cout << "EEawM: " << E_RawM << endl; 
-  for(i=0;i<E_RawM;i++)
-    {
-      a=(E_Raw_Nr[i]+1)/8;
-      b=((E_Raw_Nr[i]+1)%8)-1;
-      if(b!=-1)
+	cout << "EEawM: " << E_RawM << endl; 
+	for(i=0;i<E_RawM;i++)
 	{
-	  cout << E_Raw_Nr[i] << endl;
-      cout << "PAD " << a << " " << b <<": ";
-      cout << E_Raw[i] << " ";
-      cout << endl;
-	}    
+		aa=(E_Raw_Nr[i]+1)/8;
+		bb=((E_Raw_Nr[i]+1)%8)-1;
+		if(bb!=-1)
+		{
+			cout << E_Raw_Nr[i] << endl;
+			cout << "PAD " << aa << " " << bb <<": ";
+			cout << E_Raw[i] << " ";
+			cout << endl;
+		}    
     }
-
-
 }
+
 void IonisationChamberv::Show(void)
 {
-  Int_t i;
-  int a,b;
+	Int_t i;
+	int a,b;
 
-#ifdef DEBUG
-  cout << "IonisationChamberv::Show" << endl;
-#endif
+	#ifdef DEBUG
+	cout << "IonisationChamberv::Show" << endl;
+	#endif
 
-  cout.setf(ios::showpoint);
+	cout.setf(ios::showpoint);
  
-  cout << "Present: " << Present << endl;
-  if(Present)
-    {
-      cout << "EM: " << EM << endl;
-      for(i=0;i<24;i++)
+	cout << "Present: " << Present << endl;
+	if(Present)
 	{
-	  a=(i+1)/8;
-	  b=((i+1)%8)-1;
-	  if(b!=-1)
-	    {
-	      if(E[i] > 0) 
+		cout << "EM: " << EM << endl;
+		for(i=0;i<24;i++)
 		{
-		  cout << "ROW " << a << " PAD " << b << ": "; 
-		  cout << E[i] <<endl;
-		}	    
-	    }
+			a=(i+1)/8;
+			b=((i+1)%8)-1;
+			if(b!=-1)
+			{
+				if(E[i] > 0) 
+				{
+					cout << "ROW " << a << " PAD " << b << ": "; 
+					cout << E[i] <<endl;
+				}	    
+	    	}
+		}
+		for(i=0;i<3;i++) cout << "SUM ROW:" << i << " " << ES[i] << endl;
+		cout << "ETotal: " << ETotal << endl;
 	}
-      for(i=0;i<3;i++)
-	cout << "SUM ROW:" << i << " " << ES[i] << endl;
-      cout << "ETotal: " << ETotal << endl;
-    }
 }
 
 
--- VAMOS/detectors/IonisationChamberv.h
+++ VAMOS/detectors/IonisationChamberv.h
@@ -2,57 +2,64 @@
 #define _IONCHAMBER_CLASS
 
 #include "Rtypes.h"
-#include"Defines.h"
-#include"LogFile.h"
-#include"Random.h"
+#include "Defines.h"
+#include "LogFile.h"
+#include "Random.h"
 #include "TTree.h"
 
 class IonisationChamberv
 {
-   Bool_t Ready;
- public:
-  IonisationChamberv(LogFile *LOg);
-  virtual ~IonisationChamberv(void);
-  
-  LogFile *L;
+	Bool_t Ready;
 
-  bool Present; 
+	public:
 
-  void Init(void); 
-  void InitRaw(void); 
-  void Calibrate(void); 
-  void Show_Raw(void);
-  void Show(void);
-  void Treat(void);
-  void inAttach(TTree *inT);
-  void outAttach(TTree *outT);
-  void CreateHistograms();
-  void FillHistograms();
-  void PrintCounters(void);
+	IonisationChamberv(LogFile *LOg);
+	virtual ~IonisationChamberv(void);
+	
+	LogFile *L;
 
-  Random *Rnd;
+	bool Present; 
 
-  //energy Raw
-  UShort_t E_Raw[3*8];
-  UShort_t E_Raw_Nr[3*8];
-  Int_t E_RawM;
+	void Init(void); 
+	void InitRaw(void); 
+	void Calibrate(void); 
+	void Show_Raw(void);
+	void Show(void);
+	void Treat(void);
+	void inAttach(TTree *inT);
+	void outAttach(TTree *outT);
+	void CreateHistograms();
+	void FillHistograms();
+	void PrintCounters(void);
 
-  //Calibration coeff
-  Float_t ECoef[3][7][3];
+	Random *Rnd;
+/*
+ *	energy Raw
+ */
+	UShort_t E_Raw[3*8];
+	UShort_t E_Raw_Nr[3*8];
+	Int_t E_RawM;
+/*
+ *	Calibration coeff
+ */
+	Float_t ECoef[3][7][4];
+/*
+ *	energy Calibrated
+ */
+	Float_t E[3*8];
+	UShort_t ENr[3*8];
+	Int_t EM;
+	Float_t ERef[3][2];		// Position & energy
+	Float_t ES[3];			// Energy sum per row
+	Float_t ETotal;
+	Float_t ETresh[3];
+	UShort_t Number;
+/*
+ *	Counters
+ */
+	Int_t Counter[5];
 
-  //energy Calibrated
-  Float_t E[3*8];
-  UShort_t ENr[3*8];
-  Int_t EM;
-  Float_t ERef[3][2]; //Position & energy
-  Float_t ES[3]; //Energy sum per row
-  Float_t ETotal;
-  Float_t ETresh[3];
-
-  //Counters
-  Int_t Counter[5];
-
-  ClassDef(IonisationChamberv,0)
+	ClassDef(IonisationChamberv,0)
 };
 
 #endif
