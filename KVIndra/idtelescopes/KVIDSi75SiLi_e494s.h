d1f6f1f0d9ea8e486dc3ebb284dedb27672a8251
Guilain ADEMARD <ademard@ipnnim09>
2012-11-07 17:35:17 +0100

    - New identification telescopes for DE-E identification ChIo-Si75,   Si75-SiLi, SiLi-CsI. The X and Y coordinates used in the DE-E   identifications take into account the run-dependent pedestal corrections   of Si75 and SiLi detectors, as presented by Marie-France at the INDRA-VAMOS   meeting on 2011/07/05 (http://indra.in2p3.fr/spip/IMG/pdf/indra_06juillet2011.pdf).

---
+++ KVIndra/idtelescopes/KVIDSi75SiLi_e494s.h
@@ -0,0 +1,36 @@
+//Created by KVClassFactory on Fri Nov  2 14:11:36 2012
+//Author: Guilain ADEMARD
+
+#ifndef __KVIDSI75SILI_E494S_H
+#define __KVIDSI75SILI_E494S_H
+
+#include "KVIDSi75SiLi.h"
+#include "KVRTGIDManager.h"
+#include "KVINDRADetector.h"
+
+class KVIDSi75SiLi_e494s : public KVIDSi75SiLi, public KVRTGIDManager
+{
+
+	KVINDRADetector *fSi75; //!
+	KVINDRADetector *fSiLi; //!
+
+    Double_t GetIDMapXY(KVINDRADetector *det, Option_t *opt = "");
+
+   	public:
+   	KVIDSi75SiLi_e494s();
+   	virtual ~KVIDSi75SiLi_e494s();
+
+   	virtual void Initialize();
+
+    virtual Double_t GetIDMapX(Option_t *opt = ""); 
+    virtual Double_t GetIDMapY(Option_t *opt = "");
+
+    virtual Bool_t Identify(KVIdentificationResult *IDR, Double_t x=-1., Double_t y=-1.);
+
+    virtual Bool_t SetIdentificationParameters(const KVMultiDetArray*);
+    virtual void RemoveIdentificationParameters();
+
+   	ClassDef(KVIDSi75SiLi_e494s,1)//E503/E494S experiment INDRA identification using Si75-SiLi matrices
+};
+
+#endif
--- KVIndra/idtelescopes/KVIndraidtelescopesLinkDef.h
+++ KVIndra/idtelescopes/KVIndraidtelescopesLinkDef.h
@@ -30,5 +30,8 @@
 #pragma link C++ class KVIDChIoCorrCsI+;
 #pragma link C++ class KVIDChIoCorrCsI_e494s+;
 #pragma link C++ class KVIDSiCorrCsI+;
+#pragma link C++ class KVIDChIoSi75_e494s+;
+#pragma link C++ class KVIDSi75SiLi_e494s+;
+#pragma link C++ class KVIDSiLiCsI_e494s+;
 
 #endif
