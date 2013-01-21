0a8b97f5182b598a0e31dfcba510040aba15f8a4
John Frankland <frankland@ganil.fr>
2013-01-21 11:30:31 +0100

    adds SimReader class for ELIE events (c)2013 Dominique Durand Ltd.

--- KVMultiDet/analysis/KVMultiDetanalysisLinkDef.h
+++ KVMultiDet/analysis/KVMultiDetanalysisLinkDef.h
@@ -24,6 +24,7 @@
 #pragma link C++ class KVSimReader_SMF_asym;
 #pragma link C++ class KVSimReader_MMM;
 #pragma link C++ class KVSimReader_MMM_asym;
+#pragma link C++ class KVSimReader_ELIE;
 #pragma link C++ class KVDalitzPlot;
 #pragma link C++ class KVAutoFit;
 #pragma link C++ class KVGaus1D;																											 
