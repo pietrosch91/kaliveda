72247d12324555f00b61b1f976acb4fe84697d13
Guilain ADEMARD <ademard@ipnnim09>
2013-04-05 17:45:31 +0200

    Reduce the memory used by KVVAMOSTransferMatrix object to store  the coefficients (by a factor 15 compare to the previous commit  and by a factor 2 compare to Reconstructionv).  A object of this classe can be now saved in a ROOT file (probably  useless but funky)

--- VAMOS/reconstruction/KVVAMOSTransferMatrix.h
+++ VAMOS/reconstruction/KVVAMOSTransferMatrix.h
@@ -22,18 +22,34 @@ class KVVAMOSTransferMatrix : public KVBase
 
 	protected:
 
-		UChar_t       fOrder; // order of the matrix reconstruction
-		Double_t *****fCoef;  //[4][fOrder][fOrder][fOrder][fOrder] coefficients for each parameter (Brho, theta, phi, path)
+		UChar_t   fOrder;   // order of the matrix reconstruction
+		UShort_t  fNCoef[4];//[4] Number of coef for each parameter (Brho, theta, phi, path)
+		Double_t *fCoef [4];//![4][fNCoef[4]] coefficients for each parameter
+		UShort_t *f_ijkl[4];//![4][fNCoef[4]] indexes i,j,k,l corresponding to each parameters (4 bits for each index)
 		
 		enum {
-			kIsReady = BIT(14) //flag set when the coef. have been set up
+			kIsReady      = BIT(14),//flag set when the coef. have been set up
+			kIsCompressed = BIT(15) //flag when the arrays are compressed
 		};
 
 		void NewMatrix();
 		void DeleteMatrix();
+   		void ReadCoefInDataSet();
 		void ReadCoefficients( EReconParam par, ifstream &file );
-		void RecursiveReadOfCoef( EReconParam par, ifstream &file, UChar_t order, UChar_t *n, Int_t &coef_num, Int_t prev_i=0 );
+		void RecursiveReadOfCoef( EReconParam par, ifstream &file, UChar_t order, UShort_t *nijkl, UShort_t &NtoRead, UChar_t prev_i=0 );
 
+
+		inline UShort_t Code_ijkl( UShort_t i, UShort_t j, UShort_t k, UShort_t l){
+			UShort_t val = i + (j<<4) + (k<<8) + (l<<12);
+			return val;
+		}
+
+		inline void Decode_ijkl( UShort_t code, UShort_t &i, UShort_t &j, UShort_t &k, UShort_t &l) const{
+			i = code       & 15;
+			j = (code>>4)  & 15;
+			k = (code>>8)  & 15;
+			l = (code>>12) & 15;
+		}
 
    public:
 
@@ -44,31 +60,33 @@ class KVVAMOSTransferMatrix : public KVBase
    virtual void Copy (TObject&) const;
    void init();
 
+   void CompressArrays();
    virtual void Print(Option_t * option = "") const;
    Bool_t ReconstructFPtoLab( KVVAMOSReconTrajectory *traj);
    Bool_t ReconstructLabToFP( KVVAMOSReconTrajectory *traj);
 
-   virtual void ReadCoefInDataSet();
-   virtual void Reset();
-
-
-   void SetCoef( EReconParam par, Int_t i, Int_t j, Int_t k, Int_t l, Double_t coef);
+void SetCoef( EReconParam par, UShort_t i, UShort_t j, UShort_t k, UShort_t l, Double_t coef);
+   Bool_t SetOrder( UChar_t order ); 
 
    //------------- inline methods ------------------------------//
 
    inline UChar_t GetOrder() const{ return fOrder; }
-   inline void    SetOrder( UChar_t order ){ 
-	   ResetBit( kIsReady );
-	   if( fOrder == order ){ 
-		   Reset();
-		   return;
-	   }
-	   fOrder = order;
-	   NewMatrix();
-   }
+   
 
    inline Bool_t IsReady() const{ return TestBit( kIsReady ); }
    inline void SetIsReady( Bool_t ok = kTRUE ){ SetBit( kIsReady, ok ); }
+
+   inline UShort_t GetMaxNcoef() const{
+	   //returns the maximal number of coefficients for one parameter
+	   //from the value of the order.
+
+	   UInt_t num=1, denum=1;
+	   for(Int_t i=1; i<=4; i++){ 
+		   num*= (fOrder+i);
+		   denum*=i;
+	   }
+	   return num/denum;
+   }
 
    ClassDef(KVVAMOSTransferMatrix,1)//VAMOS's transformation matrix to map the measured coordinates at the focal plane back to the target
 };
--- VAMOS/reconstruction/VAMOSreconstructionLinkDef.h
+++ VAMOS/reconstruction/VAMOSreconstructionLinkDef.h
@@ -10,5 +10,5 @@ $Id: VAMOSLinkDef.h,v 1.4 2012/03/28 12:22:22 ademard Exp $
 #pragma link C++ class Reconstructionv+;
 #pragma link C++ class ReconstructionSeDv+;
 #pragma link C++ class KVVAMOSReconTrajectory+;
-#pragma link C++ class KVVAMOSTransferMatrix+;
+#pragma link C++ class KVVAMOSTransferMatrix-; //customized streamer
 #endif
