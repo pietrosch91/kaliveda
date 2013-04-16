edd841b14f62fb9ced3cb6ea5daf7793962c183b
John Frankland <frankland@ganil.fr>
2013-04-16 13:56:42 +0200

    adds new KVMultiDetArray class for general multidetector arrays

---
+++ KVMultiDet/geometry/KVMultiDetArray.cpp
@@ -0,0 +1,26 @@
+//Created by KVClassFactory on Tue Apr 16 09:45:50 2013
+//Author: John Frankland,,,
+
+#include "KVMultiDetArray.h"
+
+ClassImp(KVMultiDetArray)
+
+////////////////////////////////////////////////////////////////////////////////
+// BEGIN_HTML <!--
+/* -->
+<h2>KVMultiDetArray</h2>
+<h4>Axially-symmetric multidetector array</h4>
+<!-- */
+// --> END_HTML
+////////////////////////////////////////////////////////////////////////////////
+
+KVMultiDetArray::KVMultiDetArray()
+{
+   // Default constructor
+}
+
+KVMultiDetArray::~KVMultiDetArray()
+{
+   // Destructor
+}
+
---
+++ KVMultiDet/geometry/KVMultiDetArray.h
@@ -0,0 +1,19 @@
+//Created by KVClassFactory on Tue Apr 16 09:45:50 2013
+//Author: John Frankland,,,
+
+#ifndef __KVMultiDetArray_H
+#define __KVMultiDetArray_H
+
+#include "KVMultiDetArray.h"
+
+class KVMultiDetArray : public KVMultiDetArray
+{
+
+   public:
+   KVMultiDetArray();
+   virtual ~KVMultiDetArray();
+
+   ClassDef(KVMultiDetArray,1)//Base class for multidetector arrays
+};
+
+#endif
