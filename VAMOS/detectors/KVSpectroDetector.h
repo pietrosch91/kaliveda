6663714cd8cd06d3b93c1becfb7c457c1672e389
Gruyer Diego <gruyer@ganil.fr>
2013-04-25 10:40:49 +0200

    #include "TGeoMatrix.h" added to VAMOS/detectors/KVSpectroDetector.h

--- VAMOS/detectors/KVSpectroDetector.h
+++ VAMOS/detectors/KVSpectroDetector.h
@@ -7,9 +7,12 @@
 #define IDX( dir ) (Int_t)( dir -'X') 
 #define DIRECTION( idx ) (Char_t)('X'+idx) 
 
-#include "KVDetector.h"
+#include "KVDetector.h" 
 #include "TGeoVolume.h"
 #include "TGeoManager.h"
+#include "TGeoMedium.h"
+#include "TGeoMatrix.h"
+
 
 class KVSpectroDetector : public KVDetector//, public TGeoVolume
 {
