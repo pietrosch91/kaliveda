777788f1759fe8e4026eac69385b3414c61709a0
Guilain Ademard <ademard@ipno.in2p3.fr>
2013-06-12 10:06:35 +0200

    Overwriting the method KVINDRAReconDataAnalyser::preAnalysis in KVIVReconDataAnalyser in order to add the reading and setting of raw data of VAMOS as well as the ones of INDRA for the current event from  the raw data tree i.e. replacing gIndra by gMultiDetArray

--- VAMOS/analysis/KVIVReconDataAnalyser.cpp
+++ VAMOS/analysis/KVIVReconDataAnalyser.cpp
@@ -32,6 +32,25 @@ void KVIVReconDataAnalyser::preAnalysis()
 	// Read and set raw data for the current reconstructed event.
 	// Initialize VAMOS spectrometer before starting the analysis
 	// of the new event.
-	KVINDRAReconDataAnalyser::preAnalysis();
+	//
+	// This method overwrites the same method of the mother class
+	// KVINDRAReconDataAnalyser where gIndra is replaced by gMultiDetArray
+	// in order to read and set raw data of VAMOS as well as the ones of
+	// INDRA. Indeed the ACQ parameter list of gMultiDetArray contains the 
+	// parameters of both multidetectors. (for more information see the
+	// class KVINDRA_VAMOS)
+	
+	if(!theRawData) return;
+	// all recon events are numbered 1, 2, ... : therefore entry number is N-1
+	Long64_t rawEntry = fSelector->GetEventNumber() - 1;
+
+	gMultiDetArray->GetACQParams()->R__FOR_EACH(KVACQParam,Clear)();
+
+	theRawData->GetEntry(rawEntry);
+	for(int i=0; i<NbParFired; i++){
+		KVACQParam* par = gMultiDetArray->GetACQParam((*parList)[ParNum[i]]->GetName());
+		if(par) {par->SetData(ParVal[i]);}
+	}
+	
 	if(gVamos) gVamos->Initialize();
 }
