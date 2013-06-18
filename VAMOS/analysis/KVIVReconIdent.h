1b077de0d02e9b6b1b218a75a65cf0ddcc7b4e64
Guilain Ademard <ademard@ipno.in2p3.fr>
2013-06-18 10:30:48 +0200

    Replace the name of the reconstructed event branch read by KVIVReconIdent if the event is a KVIVReconEvent

--- VAMOS/analysis/KVIVReconIdent.h
+++ VAMOS/analysis/KVIVReconIdent.h
@@ -22,6 +22,17 @@ class KVIVReconIdent: public KVINDRAReconIdent {
    		virtual Bool_t Analysis();
    		virtual void EndAnalysis();
 
+		virtual void Init( TTree *tree){
+			//Before to call KVINDRAReconIdent::Init change the name of 
+			//the branch of reconstructed events which will be read  if 
+			//its class is a KVIVReconEvent and not a KVINDRAReconEvent
+			if(!tree) return;
+			TBranch *br = (TBranch *)tree->GetListOfBranches()->First();
+			if( TClass::GetClass(br->GetClassName())->InheritsFrom("KVINDRAReconEvent") )
+				SetINDRAReconEventBranchName( br->GetName() );
+			KVINDRAReconIdent::Init(tree);
+		}
+
    		ClassDef(KVIVReconIdent, 0) //Identification and reconstruction of VAMOS and INDRA events from recon data 
 };
 
