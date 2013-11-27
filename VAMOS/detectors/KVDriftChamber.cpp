ad5a6c346e8fa3105d888dd3a008cf2b95dca20f
Guilain Ademard <ademard@ipno.in2p3.fr>
2013-11-27 15:09:13 +0100

    devel. of KVDriftChamber in progress. Position determination is missing

--- VAMOS/detectors/KVDriftChamber.cpp
+++ VAMOS/detectors/KVDriftChamber.cpp
@@ -2,6 +2,7 @@
 //Author: Guilain ADEMARD
 
 #include "KVDriftChamber.h"
+#include "TH1F.h"
 
 ClassImp(KVDriftChamber)
 
@@ -13,8 +14,28 @@ ClassImp(KVDriftChamber)
 <!-- */
 // --> END_HTML
 ////////////////////////////////////////////////////////////////////////////////
+
+KVString KVDriftChamber::fACQParamTypes("0:Q, 1:E, 2:T, 9:NO_TYPE");
+KVString KVDriftChamber::fPositionTypes("0:X, 1:Y, 2:Z, 3:XY, 9:NO_TYPE");
+Int_t    KVDriftChamber::fNstripsOK = 3;
+	
 void KVDriftChamber::init(){
 	// Initialise non-persistent pointers
+	fQ  = new TH1F**[3];
+	for(Int_t i=0; i<3; i++){  //[ raw, calibrated, clean ]
+		fQ[i] = new TH1F*[2];
+		for(Int_t j=0; j<2; j++){  //[ Chamber 1, Chamber 2 ]
+			fQ[i][j]      = NULL;
+		}
+	}
+
+	fRawPos [0] = fRawPos [1] = -500;
+	fERawPos[0] = fERawPos[1] = -500;
+
+	fNstripsOK = (Int_t)gDataSet->GetDataSetEnv("KVDriftChamber.NumberOfStripsOK", 3.);
+
+	//a KVDriftChamber can not be used in a ID telescope
+	ResetBit( kOKforID );
 }
 //________________________________________________________________
 
@@ -22,7 +43,8 @@ KVDriftChamber::KVDriftChamber()
 {
    // Default constructor
    init();
-   SetType("FIL");
+   SetType("DC");
+   SetLabel("DC");
    SetName(GetArrayName());
 }
 //________________________________________________________________
@@ -41,6 +63,13 @@ KVDriftChamber::KVDriftChamber (const KVDriftChamber& obj)  : KVVAMOSDetector()
 KVDriftChamber::~KVDriftChamber()
 {
    // Destructor
+	for(Int_t i=0; i<3; i++){ // loop over [ raw, calib, clean ]
+		for(Int_t j=0; j<2; j++){ // loop over both chambers
+			SafeDelete( fQ[i][j] );
+		}
+		delete[] fQ[i];
+	}
+	delete[] fQ;
 }
 //________________________________________________________________
 
@@ -58,13 +87,293 @@ void KVDriftChamber::Copy (TObject& obj) const
 }
 //________________________________________________________________
 
+void  KVDriftChamber::Clear(Option_t *option ){
+	// Set the energy losses etc. to zero and reset the histograms used
+	// to find the positions (Qraw and Q)
+
+	KVVAMOSDetector::Clear( option );
+	ResetCalculatedData();
+}
+//________________________________________________________________
+
+void KVDriftChamber::Initialize(){
+	// Initialize the data members. Called by KVVAMOS::Initialize().
+	ResetCalculatedData();
+}
+//________________________________________________________________
+
+void KVDriftChamber::ResetCalculatedData(){
+	// Reset all position histograms (raw, calibrated, clean)
+	// and raw positions for both chambers.
+	// This method have to be called each time you read a new event (i.e.
+	// when the ACQ parameter change) otherwise certain methods 
+	// (such as GetPosition(), GetQrawHisto() ...) will not return 
+	// what you expect.
+	for(Int_t i=0; i<3; i++){
+		for(Int_t j=0; j<2; j++){
+			if(fQ[i][j]) fQ[i][j]->Reset();
+		}
+	}
+
+	fRawPos [0] = fRawPos [1]  = -500;
+	fERawPos[0] = fERawPos[1] = -500;
+}
+//________________________________________________________________
+
 const Char_t* KVDriftChamber::GetArrayName(){
 	// Name of detector given in the form
-	// FIL_1 FIL_2
-	// to be compatible with GANIL acquisition parameters.
+	// DC1 DC2 ...
 	//
 	// The root of the name is the detector type.
-	fFName = Form("%s_%d",GetType(),GetNumber());
+	
+	if( GetNumber()>0 ){
+		fFName  = Form("%s%d",GetType(),GetNumber());
+		SetLabel( fFName.Data() );
+	}
+	else fFName = GetType();
 	return fFName.Data();
 }
+//________________________________________________________________
 
+const Char_t *KVDriftChamber::GetEBaseName(){
+	// Base name of the energy used to be compatible 
+	// GANIL acquisition parameters 
+	//
+	// The base name is "EFIL_<number>"
+
+	return Form("EFIL_%d",GetNumber());
+}
+//________________________________________________________________
+
+void KVDriftChamber::SetACQParams(){
+	// Setup the acquistion parameters of electron drift time, energy and position  to the drift chamber.
+	// Electron drif time ACQ parameters have the type 'T' (for time).
+	// Energy ACQ parameters have the type 'E' (for Energy).
+	// Their name has the form: [param. type]FIL_[detector number]
+	//
+	// Position ACQ parameters have the type 'Q' (for charge).
+	// Their name has the form: STR_[detector number][chamber number]_[64]
+	// where 64 is the number of strips or the X measurment in each chamber.
+
+
+
+	// Charge ACQ parameters	
+	TString name, title;
+	KVACQParam *par = NULL;
+	Int_t Nstrips =  64;
+	const Char_t *extraname[]  = {    "raw","calib",        "clean"};
+	const Char_t *extratitle[] = {       "",     "","without noise"};
+	const Char_t *units[]      = {"channel", "Volt",         "Volt"};
+	for(Int_t i=0; i<2; i++){ // loop over row
+		Int_t c_num = i+1;
+		for(Int_t num =1; num<=Nstrips; num++){  // loop over strips
+			par = new KVACQParam;
+			name.Form("STR_%d%d_%02d",GetNumber(),c_num,num);
+			par->SetName(name);
+			par->SetType("Q");
+			par->SetLabel( Form("C%d", c_num) );
+			par->SetNumber( num );
+			par->SetUniqueID( CalculateUniqueID( par ) );
+			AddACQParam(par);
+		}
+
+		// build  Q histograms for both rows
+		for(Int_t j=0; j<3; j++){ // loop over {raw,calib,clean}
+			// Qraw histogram
+			if(fQ[j][i]) SafeDelete( fQ[j][i] );
+			name.Form("Q%s_%s_C%d",extraname[j],GetArrayName(),c_num);
+			title.Form("X position in chamber %d of %s %s; Strip number; Q%s (%s)",c_num,GetArrayName(),extratitle[j],extraname[j],units[j]);
+			fQ[j][i] = new TH1F( name.Data(), title.Data(), Nstrips, .5, Nstrips +.5);
+			fQ[j][i]->SetDirectory(0);
+			fQ[j][i]->SetLineColor(j+1);
+		}
+	}
+
+	// Energy ACQ Parameters
+	par = new KVACQParam;
+	par->SetName( GetEBaseName() );
+	par->SetType("E");
+	par->SetNumber( GetNumber() );
+	par->SetUniqueID( CalculateUniqueID( par ) );
+	AddACQParam(par);
+
+	// Electron drift time ACQ Parameters
+	par = new KVACQParam;
+	par->SetName( Form("TFIL_%d",GetNumber()) );
+	par->SetType("T");
+	par->SetNumber( GetNumber() );
+	par->SetUniqueID( CalculateUniqueID( par ) );
+	AddACQParam(par);
+}
+//________________________________________________________________
+   
+TH1F *KVDriftChamber::GetQrawHisto( Int_t c_num ){
+	// Retruns the X position histogram with raw charges (Q) for 
+	// a given chamber. c_num=[1,2] is the number of this chamber.
+	// If the histogram is empty then no acquisition parameter
+	// was fired.
+	
+	Int_t i = c_num;
+	if( (i<0) || (i>1) ) return NULL; 
+	if( !fQ[0][i] ) return NULL;
+	if(fQ[0][i]->GetEntries()) return fQ[0][i];
+	
+	fQ[0][i]->Reset();
+	TIter next(GetACQParamList());
+
+	KVACQParam *par  = NULL;
+	while((par = (KVACQParam *)next())){
+
+ 	   	if( (par->GetLabel()[1] != Char_t('1'+i) ) || !par->Fired("P") ) continue;	
+
+		fQ[0][i]->SetBinContent( par->GetNumber(), (Double_t)par->GetCoderData() );
+	}
+
+ 	return fQ[0][i];
+}
+//________________________________________________________________
+   
+TH1F *KVDriftChamber::GetQHisto( Int_t c_num ){
+	// Retruns the position histogram with calibrated charges (Q)
+	// for a given chamber. c_num=[1,2] is the number of this chamber.
+
+	Int_t i = c_num-1;
+	if( (i<0) || (i>1) ) return NULL; 
+	if( !fQ[1][i] ) return NULL;
+	if(fQ[1][i]->GetEntries()) return fQ[1][i];
+
+	fQ[1][i]->Reset();
+	TIter next(GetListOfCalibrators());
+
+	KVCalibrator *cal  = NULL;
+	KVACQParam   *par  = NULL;
+	Int_t num;
+
+	Int_t NcalOK = 0;
+	while((cal = (KVCalibrator *)next())){
+		
+
+ 	   	if( !cal->GetStatus() || (GetPositionTypeIdxFromID( cal->GetUniqueID() ) != i) ) continue;	
+		NcalOK++;
+
+		KVFunctionCal *calf = (KVFunctionCal *)cal;
+		par = calf->GetACQParam();
+		if( !par->Fired("P") ) continue;
+		
+		num =  calf->GetNumber();
+		fQ[1][i]->SetBinContent( num, calf->Compute( (Double_t)calf->GetACQParam()->GetCoderData() ) );
+	}
+
+ 	if( NcalOK < 4 ) Warning("GetQHisto","Impossible to calibrate correctly the charges for X position in the chamber %d of %s, %d calibrators are OK",c_num, GetName(), NcalOK);
+	
+ 	return fQ[1][i];
+}
+//________________________________________________________________
+
+TH1F *KVDriftChamber::GetCleanQHisto( Int_t c_num ){
+	// Retruns the X position histogram with calibrated charges (Q)
+	// after the substration of noise for a given chamber. c_num=[1,2] is the number of this chamber.
+
+	Int_t i = c_num-1;
+	if( (i<0) || (i>1) ) return NULL; 
+	if( !fQ[2][i] ) return NULL;
+	if(fQ[2][i]->GetEntries()) return fQ[2][i];
+
+	TH1F *hQcal = GetQHisto( c_num );
+	if( !hQcal ) return NULL;
+
+	Float_t threshold = CalculateQThreshold( c_num );
+
+	for(Int_t j=1; j<=hQcal->GetNbinsX(); j++){	//loop over strips
+		Double_t QTmp = hQcal->GetBinContent(j);
+		if(QTmp > threshold){
+			fQ[2][i]->SetBinContent(j,QTmp-threshold);
+		}
+	}
+
+	return fQ[2][i];
+}
+//________________________________________________________________
+   
+Float_t KVDriftChamber::CalculateQThreshold( Int_t c_num ){
+	// Calculate the noise in order to remove it on strips for X  
+	// position of a given chamber. c_num=[1,2] is the number of this
+	// chamber.
+
+	//Study the noise and the maximum value for the charge.
+	//At this stage the quantities noise_*** contain also the signal!
+	Float_t QTmp, noise_mean, noise_var, noise_stdev, noise_sum;
+	Float_t Q_max; 
+	Q_max = noise_mean = noise_var = noise_sum = noise_stdev = 0.;
+
+	UShort_t Q_max_Nr;
+	Q_max_Nr = 1000 ;	
+
+	TH1F *hQcal = GetQHisto( c_num );
+	if( !hQcal ) return 0;
+
+	for(Int_t j=1; j<=hQcal->GetNbinsX(); j++){	//loop over strips
+		QTmp = hQcal->GetBinContent(j);
+		if(QTmp >0){
+			noise_mean += QTmp;
+			noise_var  += QTmp*QTmp;
+			noise_sum  += 1.;
+		}
+		if(QTmp > Q_max){
+			Q_max    = QTmp;
+			Q_max_Nr = j;
+		}
+	}
+
+	//Study the surrounding of the maximum charge
+	Float_t max_mean, max_var, max_sum;
+	max_mean = max_var = max_sum = 0.;
+	for(Int_t j=-Border;j<=Border;j++){
+		Int_t k = Q_max_Nr+j; 
+		if( (0<k) && (k<=hQcal->GetNbinsX()) ){
+			QTmp = hQcal->GetBinContent(k);
+			if (QTmp>0){
+				max_mean  += QTmp;
+				max_var   += QTmp*QTmp;
+				max_sum   += 1.;	
+			}
+		}
+	}
+ 	//The signal of the maximum charge is removed from the quantities noise_***
+	noise_mean -= max_mean;
+	noise_var  -= max_var;
+	noise_sum  -= max_sum;
+ 	//Calculate statistical quntities related to the noise and fix the threshold
+ 	//as the mean of the noise + 1 time its standart deviation
+	if(noise_sum<2. || noise_var==0) 
+	{
+		noise_mean = noise_var = 0;
+	}
+	else 
+	{
+		noise_mean /= noise_sum;
+		noise_var   = noise_var/noise_sum - noise_mean*noise_mean;
+		noise_stdev = TMath::Sqrt(noise_var);
+	}
+	Float_t threshold = noise_mean + noise_stdev;
+
+	return threshold;
+}
+//________________________________________________________________
+
+void KVDriftChamber::ShowCleanQHisto(Int_t c_num, Option_t *opt){
+	TH1F *hh = GetCleanQHisto( c_num );
+	if( hh ) hh->Draw(opt);
+}
+//________________________________________________________________
+
+void KVDriftChamber::ShowQrawHisto(Int_t c_num, Option_t *opt){
+	TH1F *hh = GetQrawHisto( c_num );
+	if( hh ) hh->Draw(opt);
+}
+//________________________________________________________________
+
+void KVDriftChamber::ShowQHisto(Int_t c_num, Option_t *opt){
+	TH1F *hh = GetQHisto( c_num );
+	if( hh ) hh->Draw(opt);
+}
--- VAMOS/detectors/KVDriftChamber.h
+++ VAMOS/detectors/KVDriftChamber.h
@@ -3,12 +3,27 @@
 
 #ifndef __KVDRIFTCHAMBER_H
 #define __KVDRIFTCHAMBER_H
+#define Border 5
 
 #include "KVVAMOSDetector.h"
 
+class TH1F;
+
 class KVDriftChamber : public KVVAMOSDetector
 {
+	private:
+
+		static KVString fACQParamTypes; //!types of available Acquision parameters
+		static KVString fPositionTypes; //!types of available positions
+		static Int_t    fNstripsOK;     //!minimum. numbers of strips for a good position measurement 
+
+
 	protected:
+		
+		TH1F    ***fQ;     //! array of TH1F for calibrated charge [raw, calibrated, clean][Chamber 1, Chamber 2] 
+		Double_t   fRawPos[2]; //! Measured X raw position for both Chambers
+		Double_t   fERawPos[2]; //! Error of measured X raw position for both Chambers
+
 		void init();
 
    public:
@@ -17,8 +32,41 @@ class KVDriftChamber : public KVVAMOSDetector
    virtual ~KVDriftChamber();
    void Copy (TObject&) const;
 
-   virtual const Char_t* GetArrayName();
+   virtual Float_t CalculateQThreshold( Int_t c_num );
+   virtual void  Clear( Option_t *option = "" );
+
+   virtual TH1F *GetCleanQHisto( Int_t c_num );
+   virtual TH1F *GetQHisto( Int_t c_num );
+   virtual TH1F *GetQrawHisto( Int_t c_num );
+
+   virtual void  Initialize();
+   virtual const Char_t *GetArrayName();
+   virtual const Char_t *GetEBaseName();
+   virtual void  ResetCalculatedData();
+   virtual void  SetACQParams();
+
+   virtual void ShowCleanQHisto(Int_t c_num=1, Option_t *opt = "");
+   virtual void ShowQrawHisto(Int_t c_num=1, Option_t *opt = "");
+   virtual void ShowQHisto(Int_t c_num=1 , Option_t *opt = "");
+
+
+   // ------ inline functions ----------------------//
+
+   virtual KVString &GetACQParamTypes();
+   virtual KVString &GetPositionTypes();
+
    ClassDef(KVDriftChamber,1)//Drift Chamber, used at the focal plan of VAMOS
 };
+//________________________________________________________________
+
+inline KVString &KVDriftChamber::GetACQParamTypes(){
+	return fACQParamTypes;
+}
+//________________________________________________________________
+
+inline KVString &KVDriftChamber::GetPositionTypes(){
+	return fPositionTypes;
+}
+
 
 #endif
