f6b9162050c253f36b10b05eee4c9316bc268cc2
Guilain Ademard <ademard@ipno.in2p3.fr>
2013-12-09 11:57:17 +0100

    - specitic KVIVReconIdent analysis class for e503

--- VAMOS/analysis/VAMOSanalysisLinkDef.h
+++ VAMOS/analysis/VAMOSanalysisLinkDef.h
@@ -13,6 +13,7 @@ $Id: VAMOSLinkDef.h,v 1.4 2009/01/14 15:59:49 franklan Exp $
 #pragma link C++ class KVIVReconDataAnalyser+;
 #endif
 #pragma link C++ class KVIVReconIdent+;
+#pragma link C++ class KVIVReconIdent_e503+;
 #pragma link C++ class Analysisv+;
 #pragma link C++ class Analysisv_e494s+;
 #pragma link C++ class Analysisv_e503+;
