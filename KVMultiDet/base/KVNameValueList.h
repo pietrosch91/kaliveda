17966568b9a4714624337419cbdab6678fe409ea
Diego Gruyer <gruyer@ebox5.gr3.fi>
2015-02-04 16:15:21 +0100

    Ajout de l'operateur += dans KVNameValueList.

--- KVMultiDet/base/KVNameValueList.h
+++ KVMultiDet/base/KVNameValueList.h
@@ -80,6 +80,9 @@ class KVNameValueList : public TNamed
    virtual void WriteEnvFile(const Char_t* filename);
 
     void Sort() { fList.Sort(); }
+
+    KVNameValueList operator += (KVNameValueList & nvl);
+
    
 	ClassDef(KVNameValueList,3)//A general-purpose list of parameters
 };
