5f354193548d784680210ef72d2417380096f781
John Frankland <frankland@ganil.fr>
2015-02-19 11:52:46 +0100

    return to original package organisation after re-reading debian guide

--- tools/make_deb_files.sh
+++ tools/make_deb_files.sh
@@ -36,7 +36,21 @@ if [ "$3"="KVMultiDet" ]; then
 fi
 
 # libraries
+# just the .so symlinks => dev package
+devlibs=`find usr/lib -name lib$3'*'.so`
 allibs=`find usr/lib -name lib$3'*' | grep -v pcm`
+# remove from allibs any lib which is in devlibs
+for dlib in $allibs; do
+   good="yes"
+   for lib in $devlibs; do
+      if [ "$dlib" = "$lib" ]; then
+         good="no"
+      fi
+   done
+   if [ "x$good" = "xyes" ]; then
+      libs="$libs $dlib"
+   fi
+done
 
 # data
 datafiles=`ls $1/$3/data`
@@ -47,13 +61,19 @@ tmplfiles=`ls $1/$3/factory`
 # etc
 etcfiles=`ls $1/$3/etc`
 
+# cmake helper files
+cmakefiles=`find usr/lib -name '*.cmake'`
+
 # write files
 cd $1
-install_file=$1/debian/$4.install
-dev_install_file=$1/debian/$4-dev.install
-dirs_file=$1/debian/$4.dirs
-dev_dirs_file=$1/debian/$4-dev.dirs
-rm -f $install_file $dev_install_file $dirs_file $dev_dirs_file
+tool_install_file=$1/debian/$4.install
+install_file=$1/debian/lib$4.install
+dev_install_file=$1/debian/lib$4-dev.install
+tool_dirs_file=$1/debian/$4.dirs
+dirs_file=$1/debian/lib$4.dirs
+dev_dirs_file=$1/debian/lib$4-dev.dirs
+rm -f $tool_install_file $tool_dirs_file $install_file $dev_install_file $dirs_file $dev_dirs_file
+
 for file in $datafiles; do
    echo "/usr/share/kaliveda/data/$file" >> $install_file
 done
@@ -63,29 +83,39 @@ done
 for etc in $etcfiles; do
    echo "/usr/share/kaliveda/etc/$etc" >> $install_file
 done
+for lib in $devlibs; do
+   echo "/$lib" >> $dev_install_file
+done
 for header in $SUBPROJ_HEADERS; do
    echo "/usr/include/kaliveda/$header" >> $dev_install_file
 done
-for lib in $allibs; do
+for lib in $libs; do
    echo "/$lib" >> $install_file
 done
 if [ "$3"="KVMultiDet" ]; then
   extras="/usr/share/kaliveda/etc/config.files /usr/lib/libfitltg.so /usr/lib/libgan_tape.so"
-  extras="$extras /usr/bin/update_runlist /usr/bin/kvtreeanalyzer /usr/bin/KaliVeda /usr/bin/kvdatanalyser /usr/bin/KaliVedaAnalysis"
+  tools="/usr/bin/update_runlist /usr/bin/kvtreeanalyzer /usr/bin/KaliVeda /usr/bin/kvdatanalyser /usr/bin/KaliVedaAnalysis"
   dev_extras="/usr/share/kaliveda/etc/kaliveda.m4 /usr/share/kaliveda/etc/nedit.cf /usr/bin/kaliveda-config"
   for e in $extras; do
      echo "$e" >> $install_file
   done
+  for t in $tools; do
+     echo "$t" >> $tool_install_file
+  done
   for e in $dev_extras; do
      echo "$e" >> $dev_install_file
   done
+  for e in $cmakefiles; do
+     echo "/$e" >> $dev_install_file
+  done
 elif [ "$3"="KVIndra" ]; then
-  extras="/usr/bin/KaliVedaGUI /usr/bin/KVDataBaseGUI"
-  for e in $extras; do
-     echo "$e" >> $install_file
+  tools="/usr/bin/KaliVedaGUI /usr/bin/KVDataBaseGUI"
+  for t in $tools; do
+     echo "$t" >> $tool_install_file
   done
 fi
 
 # make '.dirs' files
 dirname `cat $install_file` | sort -u > $dirs_file
 dirname `cat $dev_install_file` | sort -u > $dev_dirs_file
+dirname `cat $tool_install_file` | sort -u > $tool_dirs_file
