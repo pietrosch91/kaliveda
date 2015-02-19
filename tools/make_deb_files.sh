d4bce6a96ad50feda1237cc4daa32d209305396a
John Frankland <frankland@ganil.fr>
2015-02-19 10:14:24 +0100

    new packages corresponding to base libraries (KVMultiDet): kaliveda & kaliveda-dev also adds tools/make_deb_files.sh tool to automatically generate the debian/*.dirs and debian/*.install files

--- tools/make_deb_files.sh
+++ tools/make_deb_files.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+# After doing:
+# cd $2
+# cmake $1 -DCMAKE_INSTALL_PREFIX=/usr -Dgnuinstall=yes
+# make -j4 install DESTDIR=$2/tmp
+#
 # $1 = root source directory
-# $2 = cmake build directory
+# $2 = build directory
 # $3 = subproject name (KVMultiDet, KVIndra, etc)
 # $4 = package name (libkaliveda etc.)
 
 stringContain() { [ -z "${2##*$1*}" ]; }
+
+if [ $# -lt 4 ]; then
+   echo "`basename $0` [source dir] [destdir] [subproject] [package]"
+   exit 0
+fi
 
 version=`cat $1/VERSION | sed 's/\//./'`
 
@@ -27,7 +37,7 @@ fi
 
 # libraries
 libs=`find usr/lib -name lib$3'*'.$version`
-allibs=`find usr/lib -name lib$3'*'`
+allibs=`find usr/lib -name lib$3'*' | grep -v pcm`
 # remove from allibs any lib which is in libs
 for dlib in $allibs; do
    good="yes"
@@ -52,9 +62,11 @@ etcfiles=`ls $1/$3/etc`
 
 # write files
 cd $1
-install_file=$4.install
-dev_install_file=$4-dev.install
-rm -f $install_file $dev_install_file
+install_file=$1/debian/$4.install
+dev_install_file=$1/debian/$4-dev.install
+dirs_file=$1/debian/$4.dirs
+dev_dirs_file=$1/debian/$4-dev.dirs
+
 for file in $datafiles; do
    echo "/usr/share/kaliveda/data/$file" >> $install_file
 done
@@ -65,21 +77,31 @@ for etc in $etcfiles; do
    echo "/usr/share/kaliveda/etc/$etc" >> $install_file
 done
 for lib in $devlibs; do
-   echo "$lib" >> $dev_install_file
+   echo "/$lib" >> $dev_install_file
 done
 for header in $SUBPROJ_HEADERS; do
    echo "/usr/include/kaliveda/$header" >> $dev_install_file
 done
 for lib in $libs; do
-   echo "$lib" >> $install_file
+   echo "/$lib" >> $install_file
 done
 if [ "$3"="KVMultiDet" ]; then
   extras="/usr/share/kaliveda/etc/config.files /usr/lib/libfitltg.so /usr/lib/libgan_tape.so"
+  extras="$extras /usr/bin/update_runlist /usr/bin/kvtreeanalyzer /usr/bin/KaliVeda /usr/bin/kvdatanalyser /usr/bin/KaliVedaAnalysis"
   dev_extras="/usr/share/kaliveda/etc/kaliveda.m4 /usr/share/kaliveda/etc/nedit.cf /usr/bin/kaliveda-config"
-   for e in $extras; do
-      echo "$e" >> $install_file
-   done
-   for e in $dev_extras; do
-      echo "$e" >> $dev_install_file
-   done
+  for e in $extras; do
+     echo "$e" >> $install_file
+  done
+  for e in $dev_extras; do
+     echo "$e" >> $dev_install_file
+  done
+elif [ "$3"="KVIndra" ]; then
+  extras="/usr/bin/KaliVedaGUI /usr/bin/KVDataBaseGUI"
+  for e in $extras; do
+     echo "$e" >> $install_file
+  done
 fi
+
+# make '.dirs' files
+dirname `cat $install_file` | sort -u > $dirs_file
+dirname `cat $dev_install_file` | sort -u > $dev_dirs_file
