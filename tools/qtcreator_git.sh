dc0d6b47c688c3cc0124b58c95c4ac2654fb9567
John Frankland <frankland@ganil.fr>
2015-02-25 16:58:30 +0100

    different tools for populating qtcreator project files using either bzr or git version control systems

---
+++ tools/qtcreator_git.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Set up QTCreator project files:
+# fill kaliveda.files with all source files under git control
+# fill kaliveda.includes with output of root-config --incdir and all
+#   directories under git control
+
+git ls-files --exclude-standard | \egrep '\.h$|\.cpp$|\.c$|\.cxx$|\.C$|CMakeLists.txt' > kaliveda.files
+
+echo '.' > kaliveda.includes
+dirname `find . -name '*.h'` | sort -u > kaliveda.includes
+
+# to fix a bug in QtCreator 3.0, copy all ROOT headers used by KV sources to local directory
+echo '.root-headers' >> kaliveda.includes
+
+mkdir -p .root-headers
+
+HEADERLIST=`\grep '#include [",<][R,T].*\.h[",>]' */*/*.h | awk '{print $2}' | sed 's/["<]//' | sed 's/[">]//' | sort | uniq`;
+for h in $HEADERLIST; do
+	cp -u `root-config --incdir`/$h .root-headers/;
+done
+
+HEADERLIST=`\grep '#include [",<][R,T].*\.h[",>]' */*/*.cpp | awk '{print $2}' | sed 's/["<]//' | sed 's/[">]//' | sort | uniq`;
+for h in $HEADERLIST; do
+	cp -u `root-config --incdir`/$h .root-headers/;
+done
+
+HEADERLIST=`\grep '#include [",<][R,T].*\.h[",>]' execs/*.c* | awk '{print $2}' | sed 's/["<]//' | sed 's/[">]//' | sort | uniq`;
+for h in $HEADERLIST; do
+	cp -u `root-config --incdir`/$h .root-headers/;
+done
+
