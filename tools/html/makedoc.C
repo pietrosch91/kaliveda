5f9a60e296bc406a767bbb823a1abe848eef5f9d
John Frankland <frankland@ganil.fr>
2015-10-01 18:02:29 +0200

    improves html class reference generation
    put back the 'classic' kaliveda banner & colour scheme
    plus (for classes created with KVClassFactory) name of author and
    class creation date

--- tools/html/makedoc.C
+++ tools/html/makedoc.C
@@ -20,6 +20,9 @@ void makedoc(const TString& output_dir = "htmldoc")
    h.SetViewCVS(Form("https://github.com/kaliveda-dev/kaliveda/blob/%s/",maj_vers.Data()));
    h.SetPathDefinition(KVPathDef());
    h.SetFileDefinition(KVFileDef());
+	h.SetAuthorTag("//Author:");
+	h.SetCopyrightTag("//Created");
+   h.SetHomepage("http://indra.in2p3.fr/KaliVedaDoc");
    
    // "hide" the CMakeCache.txt & install_manifest.txt files
    // or links will be generated for them under "Further reading..."
@@ -31,8 +34,29 @@ void makedoc(const TString& output_dir = "htmldoc")
    gSystem->Rename(f1,f11);
    gSystem->Rename(f2,f22);
    
-   h.MakeAll();
+   TString htmloutdir;
+   if(gSystem->IsAbsoluteFileName(output_dir)) htmloutdir = output_dir;
+   else htmloutdir = path + output_dir;
+   TString our_head = path + "header.html";
+   TString our_head_out = htmloutdir + "/header.html";
+   gSystem->CopyFile(our_head,our_head_out);
+   gSystem->Unlink(our_head);
+	h.SetHeader(our_head_out);
+   TString our_foot = path + "footer.html";
+   TString our_foot_out = htmloutdir + "/footer.html";
+   gSystem->CopyFile(our_foot,our_foot_out);
+   gSystem->Unlink(our_foot);
+	h.SetFooter(our_foot_out);
+   
+   h.MakeAll(kTRUE);
    
    gSystem->Rename(f11,f1);
    gSystem->Rename(f22,f2);
+   
+   TString our_css = path + "ROOT.css";
+   TString our_css_out = htmloutdir + "/ROOT.css";
+   gSystem->CopyFile(our_css,our_css_out);
+   TString our_banner = path + "htmldoc_banner.png";
+   TString our_banner_out = htmloutdir + "/htmldoc_banner.png";
+   gSystem->CopyFile(our_banner,our_banner_out);
 }
