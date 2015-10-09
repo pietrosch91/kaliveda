184a021b405ebef4026e805cc0fa80b9c7d319ec
John Frankland <frankland@ganil.fr>
2015-10-09 10:34:42 +0200

    ROOT astyle config file & git commit hook to apply it
    In order to automatically enforce coding conventions, we use a pre-commit hook
    which will re-format all source code before it is committed using astyle
    and the latest ROOT coding style. The hook will be set up by cmake if it is not
    in place.
    
    This means that if astyle is not installed, no source code can be committed
    (other files are not restricted)
    only prevent commits for lack of astyle if astyle should be used (source code)
    automatically set up the git pre-commit hook on first configure/build

---
+++ tools/git-hook-pre-commit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Automatically reformat any committed code using astyle
+
+ASTYLE_PARAMETERS="--suffix=none --options=./tools/astyle.rc"
+FILE_PATTERN="\.(c|cpp|cxx|C|h)$"
+
+FILES=`git diff-index --cached -r --name-only HEAD --diff-filter=ACMR |  egrep $FILE_PATTERN`
+
+if [ "x$FILES" != "x" ]; then
+
+   ASTYLE=$(which astyle)
+   if [ $? -ne 0 ]; then
+	   echo "git pre-commit hook:"
+	   echo "astyle not installed. Unable to check source file format policy."
+	   echo "You must install astyle if you want to commit your changes"
+	   exit 1
+   fi
+
+   for file in $FILES; do
+	   $ASTYLE $ASTYLE_PARAMETERS $file
+      git add $file
+   done
+
+fi
