73768ce8e870fcc929d86ef1bbf989d51904a9a1
John Frankland <frankland@ganil.fr>
2015-10-29 15:37:38 +0100

    KVGemini: retrieve any remaining excitation energy of decay products

--- KVMultiDet/events/KVGemini.cpp
+++ KVMultiDet/events/KVGemini.cpp
@@ -30,15 +30,15 @@ ClassImp(KVGemini)
 //     catch (gemini_bad_decay& e) {
 //        // bad event, could not calculate all decays
 //     }
-//        
+//
 ////////////////////////////////////////////////////////////////////////////////
 
-KVGemini::KVGemini() : KVBase("gemini++","Calculate statistical decay of excited nuclei")
+KVGemini::KVGemini() : KVBase("gemini++", "Calculate statistical decay of excited nuclei")
 {
    // Default constructor
-	// enhanceIMF=true: call CWeight::setWeightIMF() before any decay
-	//       (enhance the probabilty of IMF emission)
-	//    in this case the event weights returned by
+   // enhanceIMF=true: call CWeight::setWeightIMF() before any decay
+   //       (enhance the probabilty of IMF emission)
+   //    in this case the event weights returned by
 }
 
 KVGemini::~KVGemini()
@@ -48,135 +48,131 @@ KVGemini::~KVGemini()
 
 void KVGemini::DecaySingleNucleus(KVSimNucleus& toDecay, KVSimEvent* decayProducts)
 {
-	// Calculate decay products of excited nucleus toDecay
-	// Takes into account Z, A, E*, v and angular momentum of nucleus.
-	// Adds all decay products to decayProducts event:
-	//     - call decayProducts->Clear() before calling this method
-	//       if you just want to keep the products of a single nucleus 
-	//
-	// If there is a problem with the decay of the nucleus,
-	// we throw an exception of type gemini_bad_decay
+   // Calculate decay products of excited nucleus toDecay
+   // Takes into account Z, A, E*, v and angular momentum of nucleus.
+   // Adds all decay products to decayProducts event:
+   //     - call decayProducts->Clear() before calling this method
+   //       if you just want to keep the products of a single nucleus
+   //
+   // If there is a problem with the decay of the nucleus,
+   // we throw an exception of type gemini_bad_decay
 
-	CNucleus CN(toDecay.GetZ(),toDecay.GetA(),toDecay.GetExcitEnergy(),
-                toDecay.GetAngMom().Mag());
-	// set velocity
-	CN.setVelocityCartesian(toDecay.GetVelocity().X(),toDecay.GetVelocity().Y(),toDecay.GetVelocity().Z());
-	
-	// set angle of spin axis
-        CAngle ang(toDecay.GetAngMom().Theta(),toDecay.GetAngMom().Phi());
-	CN.setSpinAxis(ang);
-		
-	CN.decay();
-		
-	if(CN.abortEvent){//problem with decay?
-		// throw exception on gemini++ error
-		CN.reset();
-		throw gemini_bad_decay();
-	}
-	
+   CNucleus CN(toDecay.GetZ(), toDecay.GetA(), toDecay.GetExcitEnergy(),
+               toDecay.GetAngMom().Mag());
+   // set velocity
+   CN.setVelocityCartesian(toDecay.GetVelocity().X(), toDecay.GetVelocity().Y(), toDecay.GetVelocity().Z());
+
+   // set angle of spin axis
+   CAngle ang(toDecay.GetAngMom().Theta(), toDecay.GetAngMom().Phi());
+   CN.setSpinAxis(ang);
+
+   CN.decay();
+
+   if (CN.abortEvent) { //problem with decay?
+      // throw exception on gemini++ error
+      CN.reset();
+      throw gemini_bad_decay();
+   }
+
    int np = CN.getNumberOfProducts();
-   for(int i=0; i<np; i++){
+   for (int i = 0; i < np; i++) {
 
-   	CNucleus* nuc = CN.getProducts(i);
-   	KVNucleus* N = decayProducts->AddParticle();
-   	N->SetZ( nuc->iZ );
-   	N->SetA( nuc->iA );
-   	float *v = nuc->getVelocityVector();
-   	TVector3 vv(v);
-   	N->SetVelocity(vv);
+      CNucleus* nuc = CN.getProducts(i);
+      KVNucleus* N = decayProducts->AddParticle();
+      N->SetZ(nuc->iZ);
+      N->SetA(nuc->iA);
+      N->SetExcitEnergy(nuc->fEx);
+      float* v = nuc->getVelocityVector();
+      TVector3 vv(v);
+      N->SetVelocity(vv);
 
    }
 
    CN.reset();//<-- essential!
 }
-	
+
 void KVGemini::DecayEvent(const KVSimEvent* hot, KVSimEvent* cold)
 {
-	// Perform statistical decay of all nuclei in 'hot' event
-	// Takes into account Z, A, E*, v and spin of nuclei.
-	// Adds all decay products to 'cold' output event
-	//
-	// If there is a problem with the decay of any of the nuclei in the event,
-	// we throw an exception of type gemini_bad_decay
-	
-	cold->Clear();
-	KVSimNucleus* hotnuc;
-	while( (hotnuc = (KVSimNucleus*)const_cast<KVSimEvent*>(hot)->GetNextParticle()) ){
-		try {
-			DecaySingleNucleus(*hotnuc,cold);
-		}
-		catch (exception& e) {
-			// rethrow any exceptions
-			throw;
-		}
-	}
+   // Perform statistical decay of all nuclei in 'hot' event
+   // Takes into account Z, A, E*, v and spin of nuclei.
+   // Adds all decay products to 'cold' output event
+   //
+   // If there is a problem with the decay of any of the nuclei in the event,
+   // we throw an exception of type gemini_bad_decay
+
+   cold->Clear();
+   KVSimNucleus* hotnuc;
+   while ((hotnuc = (KVSimNucleus*)const_cast<KVSimEvent*>(hot)->GetNextParticle())) {
+      try {
+         DecaySingleNucleus(*hotnuc, cold);
+      } catch (exception& e) {
+         // rethrow any exceptions
+         throw;
+      }
+   }
 }
 
 void KVGemini::FillTreeWithEvents(KVSimNucleus& toDecay, Int_t nDecays, TTree* theTree, TString branchname)
 {
-	// Perform nDecays decays of nucleus toDecay and write the events
-	// containing all decay products of each decay in theTree
-	// If no branchname is given, branch is called "gemini"
+   // Perform nDecays decays of nucleus toDecay and write the events
+   // containing all decay products of each decay in theTree
+   // If no branchname is given, branch is called "gemini"
 
-	if(branchname=="") branchname="gemini";
-	KVSimEvent *decayProducts = new KVSimEvent;
-   KVEvent::MakeEventBranch(theTree,branchname,"KVSimEvent",&decayProducts);
-	
-	while( nDecays-- ){
-		decayProducts->Clear();
-		try {
-			DecaySingleNucleus(toDecay,decayProducts);
-		}
-		catch (exception& e) {
-			continue;
-		}
-		theTree->Fill();
-		std::cout << "\xd" << "Gemini++ processing, " << nDecays << " decays left ..." << std::flush;
-	}
-	std::cout << std::endl;
-	
+   if (branchname == "") branchname = "gemini";
+   KVSimEvent* decayProducts = new KVSimEvent;
+   KVEvent::MakeEventBranch(theTree, branchname, "KVSimEvent", &decayProducts);
+
+   while (nDecays--) {
+      decayProducts->Clear();
+      try {
+         DecaySingleNucleus(toDecay, decayProducts);
+      } catch (exception& e) {
+         continue;
+      }
+      theTree->Fill();
+      std::cout << "\xd" << "Gemini++ processing, " << nDecays << " decays left ..." << std::flush;
+   }
+   std::cout << std::endl;
+
 }
-	
+
 void KVGemini::FillTreeWithArrays(KVSimNucleus& toDecay, Int_t nDecays, TTree* theTree, TString mode)
 {
-	// Perform nDecays decays of nucleus toDecay and write the events
-	// containing all decay products of each decay in theTree
-	// in the form of simple arrays.
-	// mode = "EThetaPhi" (default) or "V" and decides what informations are
-	// in the arrays (see KVEvent::FillArraysV and KVEvent::FillArraysEThetaPhi)
+   // Perform nDecays decays of nucleus toDecay and write the events
+   // containing all decay products of each decay in theTree
+   // in the form of simple arrays.
+   // mode = "EThetaPhi" (default) or "V" and decides what informations are
+   // in the arrays (see KVEvent::FillArraysV and KVEvent::FillArraysEThetaPhi)
 
-	Bool_t ethetaphi = (mode=="EThetaPhi");
+   Bool_t ethetaphi = (mode == "EThetaPhi");
 #define MAX_GEMINI_ARRAY_SIZE 200
-	Int_t mult, Z[MAX_GEMINI_ARRAY_SIZE], A[MAX_GEMINI_ARRAY_SIZE];
-	Double_t x[MAX_GEMINI_ARRAY_SIZE],y[MAX_GEMINI_ARRAY_SIZE],z[MAX_GEMINI_ARRAY_SIZE];
-	KVSimEvent *decayProducts = new KVSimEvent;
-	
-	theTree->Branch("Multiplicity",&mult,"mult/I");
-	theTree->Branch("Z",Z,"Z[mult]/I");
-	theTree->Branch("A",A,"A[mult]/I");
-	if(ethetaphi){
-		theTree->Branch("Energy", x, "Energy[mult]/D");
-		theTree->Branch("Theta", y, "Theta[mult]/D");
-		theTree->Branch("Phi", z, "Phi[mult]/D");
-	}
-	else
-	{
-		theTree->Branch("Vx", x, "Vx[mult]/D");
-		theTree->Branch("Vy", y, "Vy[mult]/D");
-		theTree->Branch("Vz", z, "Vz[mult]/D");
-	}
-	while( nDecays-- ){
-		decayProducts->Clear();
-		try {
-			DecaySingleNucleus(toDecay,decayProducts);
-		}
-		catch (exception& e) {
-			continue;
-		}
-		if(ethetaphi) decayProducts->FillArraysEThetaPhi(mult,Z,A,x,y,z);
-		else decayProducts->FillArraysV(mult,Z,A,x,y,z);
-		theTree->Fill();
-		std::cout << "\xd" << "Gemini++ processing, " << nDecays << " decays left ..." << std::flush;
-	}
-	std::cout << std::endl;
+   Int_t mult, Z[MAX_GEMINI_ARRAY_SIZE], A[MAX_GEMINI_ARRAY_SIZE];
+   Double_t x[MAX_GEMINI_ARRAY_SIZE], y[MAX_GEMINI_ARRAY_SIZE], z[MAX_GEMINI_ARRAY_SIZE];
+   KVSimEvent* decayProducts = new KVSimEvent;
+
+   theTree->Branch("Multiplicity", &mult, "mult/I");
+   theTree->Branch("Z", Z, "Z[mult]/I");
+   theTree->Branch("A", A, "A[mult]/I");
+   if (ethetaphi) {
+      theTree->Branch("Energy", x, "Energy[mult]/D");
+      theTree->Branch("Theta", y, "Theta[mult]/D");
+      theTree->Branch("Phi", z, "Phi[mult]/D");
+   } else {
+      theTree->Branch("Vx", x, "Vx[mult]/D");
+      theTree->Branch("Vy", y, "Vy[mult]/D");
+      theTree->Branch("Vz", z, "Vz[mult]/D");
+   }
+   while (nDecays--) {
+      decayProducts->Clear();
+      try {
+         DecaySingleNucleus(toDecay, decayProducts);
+      } catch (exception& e) {
+         continue;
+      }
+      if (ethetaphi) decayProducts->FillArraysEThetaPhi(mult, Z, A, x, y, z);
+      else decayProducts->FillArraysV(mult, Z, A, x, y, z);
+      theTree->Fill();
+      std::cout << "\xd" << "Gemini++ processing, " << nDecays << " decays left ..." << std::flush;
+   }
+   std::cout << std::endl;
 }
