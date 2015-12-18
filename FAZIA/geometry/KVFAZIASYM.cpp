ef2b1a35099664793f384311dd28eacdaea2d234
bonnet <eric.bonnet@ganil.fr>
2015-12-18 13:52:34 +0100

    remise de la geometry correcte pour FAZIASYM

--- FAZIA/geometry/KVFAZIASYM.cpp
+++ FAZIA/geometry/KVFAZIASYM.cpp
@@ -31,7 +31,7 @@ void KVFAZIASYM::GetGeometryParameters()
 
    fNblocks = 4;
    fFDist = 80.0;
-
+   fFThetaMin = 2.1;
 }
 
 
@@ -55,19 +55,27 @@ void KVFAZIASYM::BuildFAZIA()
    Double_t theta = 0;
    Double_t phi = 0;
 
+   Double_t theta_min = fFThetaMin;//smallest lab polar angle in degrees
+   Double_t centre_hole = 2.*tan(theta_min * TMath::DegToRad()) * distance_block_cible;
+   Double_t dx = (block->GetTotalSideWithBlindage()) / 2.;
+
+   /*
    Double_t arc = block->GetTotalSideWithBlindage() / (distance_block_cible + thick_si1 / 2.);
    arc /= 2;
    arc *= TMath::RadToDeg();
+   */
    fNblocks = 4;
    //theta theorique 5.2
    //dphi trouve par tatonnement ...
+   /*
    Double_t dphi = 20;
    dphi -= 5;
 
    phi = -1 * (dphi + 90);
+   */
+   TVector3 centre;
    for (Int_t bb = 0; bb < fNblocks; bb += 1) {
 
-/*
       if (bb == 1)        centre.SetXYZ(-1 * (dx - centre_hole / 2), -dx - centre_hole / 2, distance_block_cible);
       else if (bb == 2)   centre.SetXYZ(-1 * (dx + centre_hole / 2), dx - centre_hole / 2, distance_block_cible);
       else if (bb == 3)   centre.SetXYZ(-1 * (-dx + centre_hole / 2), dx + centre_hole / 2, distance_block_cible);
@@ -77,12 +85,13 @@ void KVFAZIASYM::BuildFAZIA()
       }
       theta = centre.Theta() * TMath::RadToDeg();
       phi = centre.Phi() * TMath::RadToDeg();
-*/
-
-      phi += 90;
-      theta = 5.2;
       printf("BLK #%d => theta=%1.2lf - phi=%1.2lf\n", bb, theta, phi);
 
+      /*
+            phi += 90;
+            theta = 5.2;
+            printf("BLK #%d => theta=%1.2lf - phi=%1.2lf\n", bb, theta, phi);
+      */
       rot2.SetAngles(phi + 90., theta, 0.);
       rot1.SetAngles(-1.*phi, 0., 0.);
       h = rot2 * trans * rot1;
