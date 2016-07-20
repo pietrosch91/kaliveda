414343637ca31c0f359a68767ce397d334d67b75
Eric Bonnet <eric.bonnet@ganil.fr>
2016-07-20 13:50:02 +0200

    KVFAZIARawEvent ajout du nombre de blocks presents dans l evt d acquisition

--- FAZIA/events/KVFAZIARawEvent.h
+++ FAZIA/events/KVFAZIARawEvent.h
@@ -16,7 +16,7 @@ protected:
    TClonesArray* fSignals;//-> array where signals are storred
    KVNameValueList* fValues;//-> list of values
    Int_t fNumber; //event number
-
+   Int_t fNumberOfBlocks; //number of blocks stored in the acquisition event
 public:
    KVFAZIARawEvent(Int_t ntot);
    KVFAZIARawEvent();
@@ -37,13 +37,21 @@ public:
    {
       return fNumber;
    }
+   void SetNumberOfBlocks(Int_t num)
+   {
+      fNumberOfBlocks = num;
+   }
+   Int_t GetNumberOfBlocks() const
+   {
+      return fNumberOfBlocks;
+   }
    KVNameValueList* GetValues() const
    {
       return fValues;
    }
    const Char_t* GetFPGAEnergy(Int_t blk, Int_t qua, Int_t tel, TString signaltype, Int_t idx = 0);
 
-   ClassDef(KVFAZIARawEvent, 1) //Handle list of signals and additional information coming from acquisition file
+   ClassDef(KVFAZIARawEvent, 2) //Handle list of signals and additional information coming from acquisition file
 };
 
 #endif
