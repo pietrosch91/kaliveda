6164dd469c4b0c57a547cfbc080acbbb8c073d7e
John Frankland <frankland@ganil.fr>
2016-08-05 14:37:37 +0200

    removed double appearance of KVAvailableRunsFile in LinkDef

--- KVMultiDet/data_management/LinkDef.h
+++ KVMultiDet/data_management/LinkDef.h
@@ -12,7 +12,6 @@
 #pragma link C++ global gDataSet;
 
 #pragma link C++ class KVDataAnalysisTask+;
-#pragma link C++ class KVAvailableRunsFile+;
 #pragma link C++ class KVRunFile+;
 #pragma link C++ class KVAvailableRunsFile+;
 #pragma link C++ class KVRemoteAvailableRunsFile+;
