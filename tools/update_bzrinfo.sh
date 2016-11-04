d6bca63e7b31356e8baaaf6feded84bbb69b0452
John Frankland <frankland@ganil.fr>
2016-11-04 14:31:35 +0100

    remove obsolete scripts and tools

--- tools/update_bzrinfo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-$1 version-info --custom \
---template="#define BZR_BRANCH_NICK \"{branch_nick}\"\n#define BZR_REVISION_ID \"{revision_id}\"\n#define BZR_REVISION_DATE \"{date}\"\n#define BZR_REVISION_NUMBER {revno}\n#define BZR_BRANCH_IS_CLEAN {clean}\n" \
-> $2
-exit
