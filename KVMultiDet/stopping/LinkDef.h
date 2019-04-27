93b249d0100b94536f2a633a1a6331b465cf00e8
John Frankland <john.frankland@ganil.fr>
2019-04-27 12:40:43 +0200

    integrate directly modified version of Ricardo Yanez's 'range' code

--- KVMultiDet/stopping/LinkDef.h
+++ KVMultiDet/stopping/LinkDef.h
@@ -14,8 +14,6 @@
 #pragma link C++ class KVedaLoss+;
 #pragma link C++ class KVedaLossRangeFitter+;
 #pragma link C++ class KVedaLossInverseRangeFunction+;
-#ifdef WITH_RANGE_YANEZ
 #pragma link C++ class KVRangeYanez+;
 #pragma link C++ class KVRangeYanezMaterial+;
-#endif
 #endif
